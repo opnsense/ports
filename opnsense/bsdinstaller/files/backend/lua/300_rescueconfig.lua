1ba446b6417d7cfd4c1eeca8621bc0d810783448
Franco Fichtner <franco@lastsummer.de>
2014-11-16 11:15:31 +0100

    opnsense/bsdinstaller: revive the config importer
    It's time to call this port our own: bump to version 2.1.  :)

--- opnsense/bsdinstaller/files/backend/lua/300_rescueconfig.lua
+++ opnsense/bsdinstaller/files/backend/lua/300_rescueconfig.lua
@@ -1,5 +1,6 @@
 --
--- Copyright (c)2009 Scott Ullrich.  All rights reserved.
+-- Copyright (c) 2009 Scott Ullrich.  All rights reserved.
+-- Copyright (c) 2014 Franco Fichtner <franco@lastsummer.de>
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions
@@ -29,88 +30,85 @@
 -- POSSIBILITY OF SUCH DAMAGE.
 --
 
--- BEGIN 050_rescue_config.lua --
+-- BEGIN 300_rescueconfig.lua --
+
+local POSIX = require("posix")
 
 if not App.conf.booted_from_install_media then
 	return nil, "not booted from install media"
 end
 
 return {
-    id = "rescue_config",
-    name = _("Rescue config.xml"),
-    req_state = { "configure" },
-    short_desc = _("Rescue config.xml from hard device"),
-    effect = function()
+	id = "rescue_config",
+	name = _("Import configuration"),
+	req_state = { "configure" },
+	short_desc = _("Import the previous configuration from hard disk"),
+	effect = function()
+	local dd = StorageUI.select_disk({
+		sd = App.state.storage,
+		short_desc = _(
+			"This tool will help you import the configuration of " ..
+			"a previous hard disk installation including backups. " ..
+			"Please select a disk:"),
+		cancel_desc = _("Cancel")
+	})
 
-    local disk1
-	local success = false
+	local disk1 = dd:get_name()
 
-    App.ui:inform(_(
-        "This tool will help you recover config.xml from a hard disk installation." )
-    )
+	-- Make sure source disk containing config.xml is selected
+	if not disk1 then
+		return step:prev()
+	end
 
-    local dd = StorageUI.select_disk({
-        sd = App.state.storage,
-        short_desc = _(
-            "Select the disk containing config.xml %s ",
-            App.conf.product.name),
-        cancel_desc = _("Cancel")
-    })
-    disk1 = dd:get_name()
+	-- make sure that we have partition we reference after
+	if POSIX.stat("/dev/" .. disk1 .."s1a", "type") == nil then
+		App.ui:inform(_("Disk is not partitioned."))
+		return step:prev()
+	end
 
-    -- Make sure source disk containing config.xml is selected
-    if not disk1 then
-        return step:prev()
-    end
-
-    local cmds = CmdChain.new()
-	cmds:add("${root}bin/rm -f /tmp/config.cache");
+	local cmds = CmdChain.new()
 	cmds:add{
-		cmdline = "${root}sbin/fsck -t ufs -y /dev/${disk1}s1a ; ${root}bin/mkdir /tmp/hdrescue ; ${root}sbin/mount /dev/${disk1}s1a /tmp/hdrescue",
-		replacements = {
-			OS = App.conf.product.name,
-			disk1 = disk1
-		}
+		cmdline = "${root}sbin/fsck -t ufs -y /dev/${disk1}s1a > /dev/null",
+		replacements = { disk1 = disk1, }
 	}
-
-	if cmds:execute() then
-		success = true	
+	cmds:add("${root}bin/mkdir -p /tmp/hdrescue");
+	cmds:add{
+		cmdline = "${root}sbin/mount /dev/${disk1}s1a /tmp/hdrescue",
+		replacements = { disk1 = disk1, }
+	}
+	if not cmds:execute() then
+		return step:prev()
 	end 
 
-	if success == true then
-		success = false
-		if FileName.is_file("/tmp/hdrescue/cf/conf/config.xml") then
-			cmds = CmdChain.new()
-			cmds:add("${root}bin/cp /tmp/hdrescue/cf/conf/config.xml /cf/conf/config.xml");
-			cmds:add{
-			cmdline = "${root}sbin/umount /tmp/hdrescue",
-			replacements = {
-					OS = App.conf.product.name,
-					disk1 = disk1
-				}
-			}
-			if FileName.is_file("/tmp/config.cache") then
-				cmds:add("${root}bin/rm /tmp/config.cache");
-			end
-		    if cmds:execute() then
-				success = true
-		    end
+	cmds = CmdChain.new()
+	if POSIX.stat("/tmp/hdrescue/cf/conf", "type") == "directory" then
+		cmds:add("${root}bin/cp /tmp/hdrescue/cf/conf/config.xml /conf/config.xml");
+		if POSIX.stat("/tmp/hdrescue/cf/conf/backup", "type") == "directory" then
+			cmds:add("${root}bin/cp -r /tmp/hdrescue/cf/conf/backup /conf");
+		end
+		if POSIX.stat("/tmp/hdrescue/cf/conf/sshd", "type") == "directory" then
+			cmds:add("${root}bin/cp -r /tmp/hdrescue/cf/conf/sshd /conf");
+		end
+		if POSIX.stat("/tmp/hdrescue/cf/conf/rrd.tgz", "type") == "regular" then
+			cmds:add("${root}bin/cp /tmp/hdrescue/cf/conf/rrd.tgz /conf");
+		end
+		if POSIX.stat("/tmp/hdrescue/cf/conf/dhcpleases.tgz", "type") == "regular" then
+			cmds:add("${root}bin/cp /tmp/hdrescue/cf/conf/dhcpleases.tgz /conf");
 		end
 	end
-
-	if success == true then
-	    App.ui:inform(_(
-	        "The configuration has been rescued. If you are running this from option 99, you will need to reboot.")
-	    )
-	else 
-	    App.ui:inform(_(
-	        "config.xml was not rescued due to errors. Check /tmp/installer.log for more information.")
-	    )
+	cmds:add("${root}bin/rm -f /tmp/config.cache");
+	cmds:add("${root}sbin/umount /tmp/hdrescue");
+	if not cmds:execute() then
+		return step:prev()
 	end
+
+	App.ui:inform(_(
+		"The configuration has been sucessfully restored. You " ..
+		"may now choose to continue the installation or bring " ..
+		"up a live system by exiting this installer."
+	))
 
 	return Menu.CONTINUE
 
-    end
+	end
 }
-
-
