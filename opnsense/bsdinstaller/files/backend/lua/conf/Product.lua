b523d696b3885e01402c1401d1f0c6bb9781895d
Franco Fichtner <franco@opnsense.org>
2015-03-10 06:54:53 +0100

    opnsense/bsdinstaller: fix pkg db wipe and responsive progress
    Reports said the database was missign for custom install, going
    back to a problem we've had earlier with "Quick/Easy" as well.
    
    While there, split up /usr in individual parts to get a reponsive
    progress bar on install, even though /usr/local will be a big
    chunk nevertheless.  ;)

--- opnsense/bsdinstaller/files/backend/lua/conf/Product.lua
+++ opnsense/bsdinstaller/files/backend/lua/conf/Product.lua
@@ -47,8 +47,8 @@ install_items = {
 	"bin",
 	"boot",
 	"boot.config",
-	"cf",
 	"conf",
+	"cf",			-- compat glue
 	"dev",
 	"etc",
 	"home",
@@ -60,27 +60,38 @@ install_items = {
 	"root",
 	"sbin",
 	"sys",
-	"usr",
+	"usr/bin",
+	"usr/games",
+	"usr/include",
+	"usr/lib",
+	"usr/lib32",
+	"usr/libdata",
+	"usr/libexec",
+	"usr/local",
+	"usr/obj",
+	"usr/sbin",
+	"usr/share",
+	"usr/src",
 	"var",
 }
 
 ui_nav_control = {
-	["*/welcome"] = "ignore",           	     			-- do not show any "welcome" items
-	["*/configure_installed_system"] = "ignore", 			-- don't put these on
-	["pre_install_tasks/select_language"] = "ignore",               -- do not show language selection
-	["pre_install_tasks/configure_network"] = "ignore", 		-- no need for configuring network
-	["*/upgrade_installed_system"] = "ignore",   			-- the main menu...
-	["*/load_kernel_modules"] = "ignore", 		 			-- do not ask about loading kernel modules
-	["*/pit/configure_console"] = "ignore",   	 			-- do not ask about console
-	["*/pit/configure_network"] = "ignore",   	 			-- do not ask about network
-	["*/*netboot*"] = "ignore",						-- ignore netboot installation services
-	["*/install/select_packages"] = "ignore", 	 			-- do not do the "Select Packages" step on install
-	["*/install/confirm_install_os"] = "ignore",			-- no need to confirm os install
+	["*/welcome"] = "ignore",				-- do not show any "welcome" items
+	["*/configure_installed_system"] = "ignore",		-- don't put these on
+	["pre_install_tasks/select_language"] = "ignore",	-- do not show language selection
+	["pre_install_tasks/configure_network"] = "ignore",	-- no need for configuring network
+	["*/upgrade_installed_system"] = "ignore",		-- the main menu...
+	["*/load_kernel_modules"] = "ignore",			-- do not ask about loading kernel modules
+	["*/pit/configure_console"] = "ignore",			-- do not ask about console
+	["*/pit/configure_network"] = "ignore",			-- do not ask about network
+	["*/*netboot*"] = "ignore",				-- ignore netboot installation services
+	["*/install/select_packages"] = "ignore",		-- do not do the "Select Packages" step on install
+	["*/install/confirm_install_os"] = "ignore",		-- no need to confirm os install
 	["*/install/warn_omitted_subpartitions"] = "ignore",	-- warn that /tmp /var and friends are being ommited
-	["*/install/finished"] = "ignore",						-- no need to extra spamming
-	["*/install/select_additional_filesystems"] = "ignore", -- do not include additional filesystems prompts
-	["*/install/270_install_bootblocks.lua"] = "ignore", 	-- ignore the old boot block installer program
-	["*/configure/*"] = "ignore",             	 			-- do not configure, we've already did it.
+	["*/install/finished"] = "ignore",			-- no need to extra spamming
+	["*/install/select_additional_filesystems"] = "ignore",	-- do not include additional filesystems prompts
+	["*/install/270_install_bootblocks.lua"] = "ignore",	-- ignore the old boot block installer program
+	["*/configure/*"] = "ignore",				-- do not configure, we've already did it.
 }
 
 booted_from_install_media=true
