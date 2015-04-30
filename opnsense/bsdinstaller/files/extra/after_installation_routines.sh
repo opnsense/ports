fc6dc56fc809396d4dee55e5b4f3e16139b2de20
Franco Fichtner <franco@opnsense.org>
2015-04-30 07:48:11 +0200

    opnsense/bsdinstaller: ready for the big screen

--- opnsense/bsdinstaller/files/extra/after_installation_routines.sh
+++ opnsense/bsdinstaller/files/extra/after_installation_routines.sh
@@ -3,15 +3,17 @@
 # Let parent script know that a install really happened
 touch /tmp/install_complete
 
-mkdir -p /mnt/var/logs/installer
-cp /tmp/install.disklabel* /mnt/var/logs/installer
-cp /tmp/installer.log /mnt/var/logs/installer
-cp /tmp/new.fdisk /mnt/var/logs/installer
+# Copy all the installer logs to the new installation
+mkdir -p /mnt/var/log/installer
+cp /tmp/install.disklabel* /mnt/var/log/installer
+cp /tmp/installer.log /mnt/var/log/installer
+cp /tmp/new.fdisk /mnt/var/log/installer
 
 # Fixup permissions on installed files
 if [ -f /etc/installed_filesystem.mtree ]; then
 	mtree -U -e -q -f /etc/installed_filesystem.mtree -p /mnt/ \
 	    > /mnt/var/logs/installer/mtree.log
+	rm /mnt/etc/installed_filesystem.mtree
 fi
 
 sync
