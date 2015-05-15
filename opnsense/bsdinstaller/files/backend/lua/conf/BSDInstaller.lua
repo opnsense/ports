b8a835301cd41f1c562d935bc2093a29192069d0
Franco Fichtner <franco@opnsense.org>
2015-05-15 17:48:23 +0200

    opnsense/bsdinstaller: fix "parallel" build
    The Makefiles here are gruesome.  My thoughts: that is not how a
    Makefile is intended to be used.  Prune all things that are not
    needed.

--- opnsense/bsdinstaller/files/backend/lua/conf/BSDInstaller.lua
+++ opnsense/bsdinstaller/files/backend/lua/conf/BSDInstaller.lua
@@ -569,22 +569,7 @@ window_subpartitions = { "c" }
 -- automatically later on, and don't need to be given with _() here.
 --
 
-languages = {
-    {
-	id = "ru",
-	name = _("Russian"),
-	short_desc = _("Russian KOI8-R"),
-	vidfont = "cp866",
-	font8x8 = "cp866-8x8",
-	font8x14 = "cp866-8x14",
-	font8x16 = "cp866-8x16",
-	keymap = "ru.koi8-r",
-	scrnmap = "koi8-r2cp866",
-	language = "ru_RU.KOI8-R",
-	charset = "KOI8-R",
-	term = "cons25r"
-    }
-}
+languages = {}
 
 
 -------------------------------------------------------------------
