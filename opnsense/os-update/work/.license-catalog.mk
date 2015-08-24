3e6be3f7723c8d8cefca1e9371210a7a9de2bfb0
Franco Fichtner <franco@opnsense.org>
2015-08-24 10:57:12 +0200

    opnsense/os-update: bump to 15.7.10
    Remove work-related files, whoopsie...

--- opnsense/os-update/work/.license-catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=BSD2CLAUSE
-_LICENSE_NAME=BSD 2-clause Simplified License
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF OSI COPYFREE
-_LICENSE_DISTFILES=
