22954ca54e3c6c2cdc59f77bf0871869a260cfa0
Franco Fichtner <franco@opnsense.org>
2015-12-17 07:54:56 +0100

    Framework: sync with upstream
    Taken from: FreeBSD

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -466,10 +466,6 @@ MOZCONFIG_SED?= ${SED} ${MOZ_SED_ARGS}
 USE_BINUTILS=	# intel-gcm.s
 CFLAGS+=	-B${LOCALBASE}/bin
 LDFLAGS+=	-B${LOCALBASE}/bin
-.  if ${OPSYS} == FreeBSD && ${OSVERSION} < 1000041 && \
-	exists(/usr/lib/libcxxrt.so) && ${CXXFLAGS:M-stdlib=libc++}
-LIBS+=		-lcxxrt
-.  endif
 . endif
 .elif ${ARCH:Mpowerpc*}
 USES:=		compiler:gcc-c++11-lib ${USES:Ncompiler*c++11*}
