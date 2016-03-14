bb04892ac5ffdbd63f5e5da8f8af06c4829b665a
Franco Fichtner <franco@opnsense.org>
2016-03-14 08:50:38 +0100

    Framework: sync with upstream
    Taken from: FreeBSD

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -304,6 +304,10 @@ CFLAGS+=		-O3
 MOZ_EXPORT+=	MOZ_OPTIMIZE_FLAGS="${CFLAGS:M-O*}"
 MOZ_OPTIONS+=	--enable-optimize
 .else
+. if ${MOZILLA_VER:R:R} >= 45 && ${ARCH} == i386 && \
+  (${OSVERSION} >= 1000000 && ${OSVERSION} < 1003501)
+USES:=			compiler:c++14-lang ${USES:Ncompiler*c++11*} # XXX ports/207837
+. endif
 MOZ_OPTIONS+=	--disable-optimize
 .endif
 
