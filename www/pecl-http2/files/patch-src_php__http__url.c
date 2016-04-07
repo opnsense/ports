b246316c5fe4f39546f9d1ecec68d05ed6a40873
Franco Fichtner <franco@opnsense.org>
2016-04-07 03:50:29 +0200

    */*: sync with upstream
    Taken from: FreeBSD

--- www/pecl-http2/files/patch-src_php__http__url.c
+++ www/pecl-http2/files/patch-src_php__http__url.c
@@ -1,5 +1,5 @@
---- php_http_url.c.orig	2015-04-08 10:14:19 UTC
-+++ php_http_url.c
+--- src/php_http_url.c.orig	2016-04-05 17:53:32 UTC
++++ src/php_http_url.c
 @@ -27,6 +27,10 @@
  #	include <arpa/inet.h>
  #endif
