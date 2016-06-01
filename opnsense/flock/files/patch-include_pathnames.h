603717c01e772c0d0a7c7a77a651df05879e8517
Franco Fichtner <franco@opnsense.org>
2016-06-01 07:29:54 +0200

    opnsense/flock: clean jail build fix

--- opnsense/flock/files/patch-include_pathnames.h
+++ opnsense/flock/files/patch-include_pathnames.h
@@ -1,6 +1,6 @@
---- include/pathnames.h.orig	2016-06-01 04:01:08 UTC
+--- include/pathnames.h.orig	2016-03-09 10:44:05 UTC
 +++ include/pathnames.h
-@@ -46,7 +46,9 @@
+@@ -46,10 +46,14 @@
  #define	_PATH_MAILDIR		"/var/spool/mail"
  #endif
  #define	_PATH_MOTDFILE		"/etc/motd"
@@ -9,4 +9,9 @@
 +#endif
  #define	_PATH_VAR_NOLOGIN	"/var/run/nologin"
  
++#ifndef _PATH_LOGIN
  #define _PATH_LOGIN		"/bin/login"
++#endif
+ #define _PATH_SHUTDOWN		"/sbin/shutdown"
+ 
+ #define _PATH_TERMCOLORS_DIRNAME "terminal-colors.d"
