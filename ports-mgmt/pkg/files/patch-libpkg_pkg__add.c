e163556d2bde54c1dc57da020cd1b93530e2c760
Franco Fichtner <franco@opnsense.org>
2016-10-30 02:54:06 +0100

    ports-mgmt/pkg: fix our patch for 10.2 compat

--- ports-mgmt/pkg/files/patch-libpkg_pkg__add.c
+++ ports-mgmt/pkg/files/patch-libpkg_pkg__add.c
@@ -1,11 +1,20 @@
---- libpkg/pkg_add.c.orig	2016-07-10 16:39:12 UTC
+--- libpkg/pkg_add.c.orig	2016-10-30 01:50:01 UTC
 +++ libpkg/pkg_add.c
-@@ -206,7 +206,7 @@ set_attrs(int fd, char *path, mode_t per
-     const struct timespec *ats, const struct timespec *mts)
- {
+@@ -208,7 +208,7 @@ set_attrs(int fd, char *path, mode_t per
  
+ 	struct timeval tv[2];
+ 	int fdcwd;
 -#ifdef HAVE_UTIMENSAT
 +#ifdef HAVE_UTIMENSAT_XXX
  	struct timespec times[2];
  
  	times[0] = *ats;
+@@ -248,7 +248,7 @@ set_attrs(int fd, char *path, mode_t per
+ 	}
+ 	fchdir(fdcwd);
+ 	close(fdcwd);
+-#ifdef HAVE_UTIMENSAT
++#ifdef HAVE_UTIMENSAT_XXX
+ 	}
+ #endif
+ 
