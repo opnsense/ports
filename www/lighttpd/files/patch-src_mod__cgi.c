b339a6083f6331e237bd43af657364dff13a5447
Franco Fichtner <franco@opnsense.org>
2016-11-04 23:12:16 +0100

    www/lighttpd: sync with upstream
    Taken from: FreeBSD

--- www/lighttpd/files/patch-src_mod__cgi.c
+++ www/lighttpd/files/patch-src_mod__cgi.c
@@ -1,19 +1,13 @@
---- src/mod_cgi.c.orig	2016-10-16 10:48:26 UTC
+--- src/mod_cgi.c.orig	2016-10-31 13:16:40 UTC
 +++ src/mod_cgi.c
-@@ -32,13 +32,14 @@
- #include <signal.h>
- #include <ctype.h>
- #include <assert.h>
-+#include <sys/param.h>
+@@ -35,8 +35,9 @@
  
  #include <stdio.h>
  #include <fcntl.h>
++#include <sys/param.h>
  
--#ifdef O_CLOEXEC
-+#if defined(O_CLOEXEC) && __FreeBSD_version >= 1000032
+-#if defined(O_CLOEXEC) && (!defined(__FreeBSD__) || defined(F_DUPFD_CLOEXEC))
++#if defined(O_CLOEXEC) && (!defined(__FreeBSD__) || __FreeBSD_version >= 1000032) 
  #define pipe_cloexec(pipefd) pipe2((pipefd), O_CLOEXEC)
--#elif defined FD_CLOEXEC
-+#elif defined(FD_CLOEXEC)
+ #elif defined FD_CLOEXEC
  #define pipe_cloexec(pipefd) \
-   (   0 == pipe(pipefd) \
-    && 0 == fcntl(pipefd[0], F_SETFD, FD_CLOEXEC) \
