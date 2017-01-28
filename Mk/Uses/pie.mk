3cdb5088c31710bffc6c1561179daadc65562a3a
Franco Fichtner <franco@opnsense.org>
2017-01-28 12:05:29 +0100

    Framework: address small issues in PIE hardening
    o pkg would not receive a PIE option marker.
    
    o suricata/pkg poisoned the global CONFIGURE_ARGS and
      would have rather need to use PIE_CONFIGURE_ARGS.  But
      since those options are set in pie.mk, just move them
      there to achieve the same.

--- Mk/Uses/pie.mk
+++ Mk/Uses/pie.mk
@@ -14,4 +14,19 @@ CFLAGS+=	-fPIC -fPIE
 CXXFLAGS+=	-fPIC -fPIE
 LDFLAGS+=	-pie
 
+.if "${PORTNAME}" == "suricata"
+# Note: Suricata on HardenedBSD supports using thread-local
+# storage. But for some reason, the linker on OPNsense doesn't like
+# it. Disable thread-local storage on OPNsense. Thread-local storage
+# is used for speed. Suricata's Victor Julien does not know
+# performance of thread-local storage versus posix thread storage.
+# OpenBSD doesn't support the __thread gnu-ism, so they use posix
+# thread storage.
+#
+# Discussed with: Victor Julien
+CONFIGURE_ARGS+=	--enable-pie --disable-threading-tls
+.elif "${PORTNAME}" == "pkg"
+CONFIGURE_ARGS+=	--enable-pie
+.endif
+
 .endif
