9ffda78d4e18b7d402a45da7562858e4599af8dc
Franco Fichtner <franco@opnsense.org>
2017-04-08 17:18:40 +0200

    www/phalcon: 3.1.2, PHP 7.1 support, fixes for 32 bit builds

--- www/phalcon/files/extra-patch-phalcon.zep.c
+++ www/phalcon/files/extra-patch-phalcon.zep.c
@@ -1,6 +1,6 @@
---- phalcon.zep.c.orig	2016-11-26 21:03:16 UTC
+--- phalcon.zep.c.orig	2017-04-05 21:26:32 UTC
 +++ phalcon.zep.c
-@@ -2114,8 +2114,8 @@ static int ZEPHIR_FASTCALL zephir_memory
+@@ -2117,8 +2117,8 @@ static int ZEPHIR_FASTCALL zephir_memory
  #define ZEPHIR_MM_RESTORE() zephir_memory_restore_stack(NULL)
  
  #else
@@ -11,7 +11,16 @@
  
  #define ZEPHIR_MM_GROW() zephir_memory_grow_stack()
  #define ZEPHIR_MM_RESTORE() zephir_memory_restore_stack()
-@@ -4212,7 +4212,7 @@ static void ZEPHIR_FASTCALL zephir_memor
+@@ -2131,7 +2131,7 @@ static int ZEPHIR_FASTCALL zephir_memory
+ static void ZEPHIR_FASTCALL zephir_memory_observe(zval *var);
+ static void ZEPHIR_FASTCALL zephir_memory_alloc(zval *var);
+ 
+-static int ZEPHIR_FASTCALL zephir_clean_restore_stack();
++static int zephir_clean_restore_stack();
+ 
+ #define zephir_safe_zval_ptr_dtor(pzval)
+ 
+@@ -4240,7 +4240,7 @@ static void ZEPHIR_FASTCALL zephir_memor
  
  #else
  
@@ -20,7 +29,7 @@
  {
  	zend_zephir_globals_def *g = ZEPHIR_VGLOBAL;
  	if (g->start_memory == NULL) {
-@@ -4221,7 +4221,7 @@ static void ZEPHIR_FASTCALL zephir_memor
+@@ -4249,7 +4249,7 @@ static void ZEPHIR_FASTCALL zephir_memor
  	zephir_memory_grow_stack_common(g);
  }
  
@@ -29,3 +38,14 @@
  {
  	zephir_memory_restore_stack_common(ZEPHIR_VGLOBAL);
  	return SUCCESS;
+@@ -4496,8 +4496,8 @@ static void ZEPHIR_FASTCALL zephir_memor
+ 	ZVAL_NULL(var);
+ }
+ 
+-static int ZEPHIR_FASTCALL zephir_clean_restore_stack() {
+-
++static int zephir_clean_restore_stack()
++{
+ 	zend_zephir_globals_def *zephir_globals_ptr = ZEPHIR_VGLOBAL;
+ 
+ 	while (zephir_globals_ptr->active_memory != NULL) {
