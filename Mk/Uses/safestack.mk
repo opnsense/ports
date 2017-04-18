c9d3eee6d7be5360332450a17de3ed3f3f3812f2
Shawn Webb <shawn@opnsense.org>
2017-04-18 11:05:26 -0400

    Add SafeStack USES flag.
    Signed-off-by:	Shawn Webb <shawn@opnsense.org>

---
+++ Mk/Uses/safestack.mk
@@ -0,0 +1,19 @@
+# $FreeBSD$
+#
+# Compile a port with SafeStack
+#
+# Feature:      safestack
+# Usage:        USES=safestack
+#
+# MAINTAINER:	shawn.webb@hardenedbsd.org
+
+.if !defined(_INCLUDE_USES_SAFESTACK_MK)
+_INCLUDE_USES_SAFESTACK_MK=    yes
+.if ${ARCH} == "amd64"
+
+CFLAGS+=	-fsanitize=safe-stack
+CXXFLAGS+=	-fsanitize=safe-stack
+LDFLAGS+=	-fsanitize=safe-stack
+
+.endif
+.endif
