4ab79c1cc014abf55e07344cf6b595ea58ebccc4
Franco Fichtner <franco@opnsense.org>
2017-07-02 06:13:57 +0200

    Hardening: remove Suricata workaround for FreeBSD 10

--- Mk/Uses/pie.mk
+++ Mk/Uses/pie.mk
@@ -11,19 +11,6 @@
 .if !defined(_INCLUDE_USES_PIE_MK)
 _INCLUDE_USES_PIE_MK=    yes
 
-.if "${PORTNAME}" == "suricata"
-# Note: Suricata on HardenedBSD supports using thread-local
-# storage. But for some reason, the linker on OPNsense doesn't like
-# it. Disable thread-local storage on OPNsense. Thread-local storage
-# is used for speed. Suricata's Victor Julien does not know
-# performance of thread-local storage versus posix thread storage.
-# OpenBSD doesn't support the __thread gnu-ism, so they use posix
-# thread storage.
-#
-# Discussed with: Victor Julien
-CONFIGURE_ARGS+=	--disable-threading-tls
-.endif
-
 .if ${pie_ARGS:Mconfigure}
 CONFIGURE_ARGS+=	--enable-pie
 .else
