b2f0ca50aaec9073fa5d4aeb40d0c0be447f47d3
Franco Fichtner <franco@opnsense.org>
2017-10-03 12:42:48 +0200

    Framework: partially sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.hardening.mk
+++ Mk/bsd.hardening.mk
@@ -234,7 +234,13 @@ OPTIONS_GROUP_HARDENING+=SAFESTACK
 ###################
 
 .if ${HARDENING_OFF:Mcfi} == ""
-.if ${OSVERSION} >= 1200020 && ${LLD_IS_LD} == "yes" && ${ARCH} == "amd64"
+
+.if ${OSVERSION} >= 1200020 && ${ARCH} == "amd64" && \
+	${LLD_IS_LD} == "yes" && \
+	${LLVM_AR_IS_AR} == "yes" && \
+	${LLVM_RANLIB_IS_RANLIB} == "yes" && \
+	${LLVM_NM_IS_NM} == "yes" && \
+	${LLVM_OBJDUMP_IS_OBJDUMP} == "yes"
 
 cfi_ARGS?=
 
