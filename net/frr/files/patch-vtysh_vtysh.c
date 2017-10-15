5f90b819dec51a491c328c2a349ab6070f748a24
Franco Fichtner <franco@opnsense.org>
2017-10-15 09:50:18 +0200

    net/frr: update to 3.0

--- net/frr/files/patch-vtysh_vtysh.c
+++ net/frr/files/patch-vtysh_vtysh.c
@@ -1,11 +1,11 @@
---- vtysh/vtysh.c.orig	2017-06-27 14:48:26 UTC
+--- vtysh/vtysh.c.orig	2017-10-13 23:17:26 UTC
 +++ vtysh/vtysh.c
-@@ -261,7 +261,7 @@ vtysh_pager_init (void)
-   if (pager_defined)
-     vtysh_pager_name = strdup (pager_defined);
-   else
--    vtysh_pager_name = strdup ("more");
-+    vtysh_pager_name = strdup ("cat");
+@@ -249,7 +249,7 @@ void vtysh_pager_init(void)
+ 	if (pager_defined)
+ 		vtysh_pager_name = strdup(pager_defined);
+ 	else
+-		vtysh_pager_name = strdup("more");
++		vtysh_pager_name = strdup("cat");
  }
  
  /* Command execution over the vty interface. */
