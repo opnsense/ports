0df0f3bf708ffe07e5de14b84d0643dafa1320c5
Franco Fichtner <franco@opnsense.org>
2017-10-21 12:11:13 +0200

    net/frr: sync with upstream
    Taken from: HardenedBSD

--- net/frr/files/patch-vtysh_vtysh.c
+++ net/frr/files/patch-vtysh_vtysh.c
@@ -1,6 +1,6 @@
---- vtysh/vtysh.c.orig	2017-10-13 23:17:26 UTC
-+++ vtysh/vtysh.c
-@@ -249,7 +249,7 @@ void vtysh_pager_init(void)
+--- vtysh/vtysh.c.orig	2017-08-09 12:56:09.000000000 +0200
++++ vtysh/vtysh.c	2017-08-22 22:04:36.068181000 +0200
+@@ -249,7 +249,7 @@
  	if (pager_defined)
  		vtysh_pager_name = strdup(pager_defined);
  	else
