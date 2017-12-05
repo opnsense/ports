d323aaedec3b83f1a5fb78526f440cf40fc25e2c
Franco Fichtner <franco@opnsense.org>
2017-12-05 21:24:49 +0100

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -5,8 +5,8 @@ SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .if !defined(SSP_UNSAFE) && \
     (${ARCH} == i386 || ${ARCH} == amd64)
-# Overridable as a user may want to use -fstack-protector-all
-SSP_CFLAGS?=	-fstack-protector
+# Overridable as a user may want to use -fstack-protector
+SSP_CFLAGS?=	-fstack-protector-all
 CFLAGS+=	${SSP_CFLAGS}
 LDFLAGS+=	-fstack-protector
 # -lssp_nonshared is needed on i386 where /usr/lib/libc.so is not an ldscript
