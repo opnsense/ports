ba05866baf2e59b105413784d612e07b4d9430b1
Franco Fichtner <franco@opnsense.org>
2018-06-26 07:17:31 +0200

    */*: sync with upstream
    Taken from: HardenedBSD

--- dns/dnstable-convert/files/patch-dnstable__convert.c
+++ dns/dnstable-convert/files/patch-dnstable__convert.c
@@ -1,6 +1,6 @@
---- src/dnstable_lookup.c.orig	2014-04-02 00:26:43 UTC
-+++ src/dnstable_lookup.c
-@@ -23,6 +23,7 @@
+--- dnstable_convert.c.orig	2018-06-05 14:55:58 UTC
++++ dnstable_convert.c
+@@ -24,6 +24,7 @@
  #include <stdio.h>
  #include <string.h>
  #include <unistd.h>
