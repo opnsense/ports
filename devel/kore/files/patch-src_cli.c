86156bcbfa33bc7f8280c5f49de281356fd75afd
Franco Fichtner <franco@opnsense.org>
2018-07-20 11:11:55 +0200

    */*: sync with upstream
    Taken from: HardenedBSD

--- devel/kore/files/patch-src_cli.c
+++ devel/kore/files/patch-src_cli.c
@@ -1,24 +1,24 @@
---- src/cli.c.orig	2018-07-09 12:06:06 UTC
+--- src/cli.c.orig	2018-07-18 14:25:49 UTC
 +++ src/cli.c
-@@ -795,7 +795,7 @@ cli_info(int argc, char **argv)
+@@ -800,7 +800,7 @@ cli_info(int argc, char **argv)
  		printf("kore source  \t %s\n", bopt->kore_source);
  	} else {
  		cli_kore_features(bopt, &features, &len);
--		printf("kore binary  \t %s/bin/kore\n", PREFIX);
-+		printf("kore binary  \t %s/bin/kore%%SUFFIX%%\n", PREFIX);
+-		printf("kore binary  \t %s/bin/kore\n", prefix);
++		printf("kore binary  \t %s/bin/kore%%SUFFIX%%\n", prefix);
  		printf("kore features\t %.*s\n", (int)len, features);
  		free(features);
  	}
-@@ -1532,7 +1532,7 @@ cli_run_kore(void)
+@@ -1537,7 +1537,7 @@ cli_run_kore(void)
  		(void)cli_vasprintf(&cmd, "./%s", appl);
  	} else {
  		flags = "-fnrc";
--		(void)cli_vasprintf(&cmd, "%s/bin/kore", PREFIX);
-+		(void)cli_vasprintf(&cmd, "%s/bin/kore%%SUFFIX%%", PREFIX);
+-		(void)cli_vasprintf(&cmd, "%s/bin/kore", prefix);
++		(void)cli_vasprintf(&cmd, "%s/bin/kore%%SUFFIX%%", prefix);
  		(void)cli_vasprintf(&cpath, "conf/%s.conf", appl);
  	}
  
-@@ -1833,6 +1833,9 @@ cli_build_flags_common(struct buildopt *bopt, struct c
+@@ -1838,6 +1838,9 @@ cli_build_flags_common(struct buildopt *bopt, struct c
  	else
  		cli_buf_appendf(buf, "-I%s/include ", bopt->kore_source);
  
@@ -28,12 +28,12 @@
  #if defined(__MACH__)
  	/* Add default openssl include path from homebrew / ports under OSX. */
  	cli_buf_appendf(buf, "-I/opt/local/include ");
-@@ -1991,7 +1994,7 @@ cli_kore_features(struct buildopt *bopt, char **out, s
+@@ -1996,7 +1999,7 @@ cli_kore_features(struct buildopt *bopt, char **out, s
  	if (bopt->single_binary) {
  		(void)cli_vasprintf(&path, ".objs/features");
  	} else {
--		(void)cli_vasprintf(&path, "%s/share/kore/features", PREFIX);
-+		(void)cli_vasprintf(&path, "%s/share/kore%%SUFFIX%%/features", PREFIX);
+-		(void)cli_vasprintf(&path, "%s/share/kore/features", prefix);
++		(void)cli_vasprintf(&path, "%s/share/kore%%SUFFIX%%/features", prefix);
  	}
  
  	cli_file_open(path, O_RDONLY, &fd);
