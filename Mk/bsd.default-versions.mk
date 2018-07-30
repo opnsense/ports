9038a7fe7930bcd4abca6fe2a09352b5c3cb1d15
Franco Fichtner <franco@opnsense.org>
2018-07-30 06:55:58 +0200

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -42,8 +42,8 @@ FIREBIRD_DEFAULT?=	2.5
 FORTRAN_DEFAULT?=	gfortran
 # Possible values: 3.0.4
 FPC_DEFAULT?=		3.0.4
-# Possible values: 4.9, 5, 6, 7
-GCC_DEFAULT?=		6
+# Possible values: 4.9, 5, 6, 7, 8
+GCC_DEFAULT?=		7
 # Possible values: 7, 8, 9, agpl
 GHOSTSCRIPT_DEFAULT?=	agpl
 # Possible values: 1.8.2
