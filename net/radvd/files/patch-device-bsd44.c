485e6a1a57dd8fb340f9c802079f3381b5578bfa
Franco Fichtner <franco@opnsense.org>
2018-07-31 05:07:47 +0200

    net/radvd: sync with upstream
    Taken from: HardenedBSD

--- net/radvd/files/patch-device-bsd44.c
+++ net/radvd/files/patch-device-bsd44.c
@@ -1,13 +1,16 @@
---- device-bsd44.c.orig	2014-07-21 07:21:42 UTC
+--- device-bsd44.c.orig	2017-06-29 04:32:29 UTC
 +++ device-bsd44.c
-@@ -171,6 +171,24 @@ int setup_linklocal_addr(struct Interfac
+@@ -126,7 +126,28 @@ ret:
+ 	return -1;
+ }
  
- int setup_allrouters_membership(struct Interface *iface)
- {
+-int setup_allrouters_membership(int sock, struct Interface *iface) { return 0; }
++int setup_allrouters_membership(int sock, struct Interface *iface) 
++{
 +	struct ipv6_mreq mreq;
 +
 +	memset(&mreq, 0, sizeof(mreq));
-+	mreq.ipv6mr_interface = iface->if_index;
++	mreq.ipv6mr_interface = iface->props.if_index;
 +
 +	/* all-routers multicast address */
 +	if (inet_pton(AF_INET6, "ff02::2",
@@ -15,13 +18,15 @@
 +		flog(LOG_ERR, "inet_pton failed");
 +		return (-1);
 +	}
-+
++       
 +	if (setsockopt(sock, IPPROTO_IPV6, IPV6_JOIN_GROUP,
 +			&mreq, sizeof(mreq)) < 0) {
-+		flog(LOG_ERR, "can't join ipv6-allrouters on %s", iface->Name);
++		flog(LOG_ERR, "can't join ipv6-allrouters on %s", iface->props.name);
 +		return (-1);
 +	}
 +
- 	return 0;
- }
++	return 0; 
++}
  
+ int set_interface_linkmtu(const char *iface, uint32_t mtu)
+ {
