ea8631aad09665bb26439ec107c8b6ced558c14d
Franco Fichtner <franco@opnsense.org>
2018-10-15 17:35:46 +0200

    */*: sync with upstream
    Taken from: HardenedBSD

--- net-im/libmesode/files/patch-src_tls__openssl.c
+++ net-im/libmesode/files/patch-src_tls__openssl.c
@@ -1,4 +1,4 @@
---- src/tls_openssl.c.orig	2018-10-08 11:40:35 UTC
+--- src/tls_openssl.c.orig	2018-09-18 18:30:27 UTC
 +++ src/tls_openssl.c
 @@ -47,7 +47,7 @@ static void _tls_log_error(xmpp_ctx_t *c
  
@@ -27,3 +27,12 @@
      alg_nid = OBJ_obj2nid(cert->sig_alg->algorithm);
  #else
      const X509_ALGOR *palg;
+@@ -330,7 +330,7 @@ tls_t *tls_new(xmpp_conn_t *conn)
+         /* Trust server's certificate when user sets the flag explicitly. */
+         mode = conn->tls_trust ? SSL_VERIFY_NONE : SSL_VERIFY_PEER;
+         SSL_set_verify(tls->ssl, mode, 0);
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
+         /* Hostname verification is supported in OpenSSL 1.0.2 and newer. */
+         X509_VERIFY_PARAM *param = SSL_get0_param(tls->ssl);
+ 
