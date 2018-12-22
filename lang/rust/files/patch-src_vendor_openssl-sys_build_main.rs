31e12566317de6bc4b48a52e813486a3641b8356
Franco Fichtner <franco@opnsense.org>
2018-12-22 02:09:10 +0100

    */*: sync with upstream
    Taken from: HardenedBSD

--- lang/rust/files/patch-src_vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-src_vendor_openssl-sys_build_main.rs
@@ -1,4 +1,5 @@
 https://github.com/sfackler/rust-openssl/commit/9fd7584a8416
+https://github.com/sfackler/rust-openssl/commit/e75793e8962d
 
 --- src/vendor/openssl-sys/build/main.rs.orig	2018-12-05 01:24:32 UTC
 +++ src/vendor/openssl-sys/build/main.rs
@@ -45,13 +46,21 @@ https://github.com/sfackler/rust-openssl/commit/9fd7584a8416
      use std::process::{self, Command};
  
      use super::env;
-@@ -500,7 +503,8 @@ See rust-openssl README for more information:
+@@ -500,6 +503,8 @@ See rust-openssl README for more information:
              (7, _) => ('7', 'x'),
              (8, 0) => ('8', '0'),
              (8, 1) => ('8', '1'),
--            _ => version_error(),
 +            (8, _) => ('8', 'x'),
-+            _ => ('8', 'x'),
++            (9, 0) => ('9', '0'),
+             _ => version_error(),
          };
  
-         println!("cargo:libressl=true");
+@@ -540,7 +545,7 @@ fn version_error() -> ! {
+         "
+ 
+ This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
+-through 2.8.1, but a different version of OpenSSL was found. The build is now aborting
++through 2.9.0, but a different version of OpenSSL was found. The build is now aborting
+ due to this version mismatch.
+ 
+ "
