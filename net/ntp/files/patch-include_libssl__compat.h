2be8a929049478dd6c997134c5d02ef967260046
Franco Fichtner <franco@opnsense.org>
2019-02-04 15:09:04 +0100

    net/ntp: LibreSSL 2.8 fix

--- net/ntp/files/patch-include_libssl__compat.h
+++ net/ntp/files/patch-include_libssl__compat.h
@@ -9,3 +9,17 @@
  /* ----------------------------------------------------------------- */
  
  # include <openssl/objects.h>
+@@ -107,10 +107,13 @@ extern int	sslshim_X509_get_signature_ni
+ 
+ #define OpenSSL_version_num	SSLeay
+ #define OpenSSL_version		SSLeay_version
++
++#if !defined(LIBRESSL_VERSION_NUMBER) || LIBRESSL_VERSION_NUMBER < 0x20800000L
+ #define X509_get0_notBefore	X509_get_notBefore
+ #define X509_getm_notBefore	X509_get_notBefore
+ #define X509_get0_notAfter	X509_get_notAfter
+ #define X509_getm_notAfter	X509_get_notAfter
++#endif
+ 
+ /* ----------------------------------------------------------------- */
+ #endif /* OPENSSL_VERSION_NUMBER < v1.1.0 */
