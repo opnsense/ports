7381b2e65d575a6e4a22ccd0576b2a546109984f
Franco Fichtner <franco@opnsense.org>
2019-02-05 15:21:48 +0100

    www/squid3: improve patch for LibreSSL to not break OpenSSL

--- www/squid3/files/patch-src_ssl_support.cc
+++ www/squid3/files/patch-src_ssl_support.cc
@@ -1,11 +1,15 @@
---- src/ssl/support.cc.orig	2019-02-04 17:47:17 UTC
+--- src/ssl/support.cc.orig	2018-07-15 20:46:55 UTC
 +++ src/ssl/support.cc
-@@ -2159,7 +2159,7 @@ remove_session_cb(SSL_CTX *, SSL_SESSION
+@@ -2159,7 +2159,11 @@ remove_session_cb(SSL_CTX *, SSL_SESSION
  }
  
  static SSL_SESSION *
 -get_session_cb(SSL *, unsigned char *sessionID, int len, int *copy)
-+get_session_cb(SSL *, const unsigned char *sessionID, int len, int *copy)
++get_session_cb(SSL *,
++#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20800000L
++const
++#endif
++unsigned char *sessionID, int len, int *copy)
  {
      if (!SslSessionCache)
          return NULL;
