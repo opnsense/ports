355e71e822926c0d2a01c5131ab10cab5dfa139f
Franco Fichtner <franco@opnsense.org>
2019-02-12 14:22:11 +0100

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -54,11 +54,11 @@ _KDE_RELNAME=		KDE${_KDE_VERSION}
 
 # === VERSIONS OF THE DIFFERENT COMPONENTS =====================================
 # Current KDE desktop.
-KDE_PLASMA_VERSION?=		5.14.5
+KDE_PLASMA_VERSION?=		5.15.0
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.54.0
+KDE_FRAMEWORKS_VERSION?=	5.55.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
