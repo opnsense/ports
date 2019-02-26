db8a7d9ac60d7201ab62e553bfa601e733248516
Franco Fichtner <franco@opnsense.org>
2019-02-26 08:30:01 +0100

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.wx.mk
+++ Mk/bsd.wx.mk
@@ -111,9 +111,9 @@ _WX_Definitions_Done=	yes
 
 _WX_COMPS_ALL=		wx contrib python
 _WX_DEP_TYPES_ALL=	build lib run
-_WX_VERS_ALL=		2.8 3.0
-_WX_VERS_UC_ALL=	2.8 3.0
-_WX_VERS_SKIP=		3.0
+_WX_VERS_ALL=		2.8 3.0 3.1
+_WX_VERS_UC_ALL=	2.8 3.0 3.1
+_WX_VERS_SKIP=		3.0 3.1
 _WX_VERS_LISTS=		WANT_WX_VER WITH_WX_VER _WX_VER_INSTALLED
 
 #
@@ -140,7 +140,7 @@ _WX_PORT_wx_3.0=	x11-toolkits/wxgtk30
 _WX_LIB_wx_3.0=		wx_baseu-3.0
 
 _WX_PORT_python_3.0=	x11-toolkits/py-wxPython30
-_WX_FILE_python_3.0=	${PYTHON_SITELIBDIR}/wx-3.0-gtk2/wx/__init__.py
+_WX_FILE_python_3.0=	${PYTHON_SITELIBDIR}/wx-3.0-gtk3/wx/__init__.py
 
 # Set _WX_SHVER_comp_ver to 0 and _WX_FILE_comp_ver for libs appropiately.
 # Set _WX_DEPTYPE_comp_ver for "python" to "run", and others to "lib".
@@ -405,8 +405,14 @@ _WX_VER=		${ver}
 # Set variables.
 #
 
-WX_CONFIG?=		${LOCALBASE}/bin/wxgtk2${_WX_UC}-${_WX_VER}-config
-WXRC_CMD?=		${LOCALBASE}/bin/wxrc-gtk2${_WX_UC}-${_WX_VER}
+.if ${_WX_VER:R} == 3
+_GTKVER=	3
+.else
+_GTKVER=	2
+.endif
+
+WX_CONFIG?=		${LOCALBASE}/bin/wxgtk${_GTKVER}${_WX_UC}-${_WX_VER}-config
+WXRC_CMD?=		${LOCALBASE}/bin/wxrc-gtk${_GTKVER}${_WX_UC}-${_WX_VER}
 WX_VERSION?=		${_WX_VER}
 
 .endif		# _WX_Need_Version
