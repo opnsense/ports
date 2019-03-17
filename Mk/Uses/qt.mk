bd9079f8958ec26c258739e69846a59ae9334ec3
Franco Fichtner <franco@opnsense.org>
2019-03-17 10:20:22 +0100

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -28,8 +28,7 @@
 _QT_MK_INCLUDED=	qt.mk
 
 # Qt versions currently supported by the framework.
-_QT_SUPPORTED?=		4 5
-QT4_VERSION?=		4.8.7
+_QT_SUPPORTED?=		5
 QT5_VERSION?=		5.12.1
 
 # We accept the Qt version to be passed by either or all of the three mk files.
@@ -304,13 +303,13 @@ paths_PATH=		${LOCALBASE}/${QT_BINDIR_REL}/qtpaths
 pixeltool_PORT=		graphics/${_QT_RELNAME}-pixeltool
 pixeltool_PATH=		${LOCALBASE}/${QT_BINDIR_REL}/pixeltool
 
-phonon_PORT=		multimedia/phonon@${_QT_RELNAME}
+phonon_PORT=		multimedia/phonon
 phonon_LIB=		libphonon.so
 
-phonon4_PORT=		multimedia/phonon@${_QT_RELNAME}
+phonon4_PORT=		multimedia/phonon
 phonon4_LIB=		libphonon4${_QT_RELNAME}.so
 
-phonon-gst_PORT=	multimedia/phonon-gstreamer@${_QT_RELNAME}
+phonon-gst_PORT=	multimedia/phonon-gstreamer
 phonon-gst_PATH=	${LOCALBASE}/${QT_PLUGINDIR_REL}/phonon_backend/libphonon_gstreamer.so
 
 plugininfo_PORT=	sysutils/${_QT_RELNAME}-qtplugininfo
