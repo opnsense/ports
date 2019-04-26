7b160c1de6fd7697c5dccf47890850426cb76982
Franco Fichtner <franco@opnsense.org>
2019-04-26 08:32:05 +0200

    Framework: seems to make little sense to set LDFLAGS=$CFLAGS
    -fstack-protector-all in LDFLAGS breaks configure stages of
    ports.  We used to have -fstack-protector in there with was
    probably silently ignored and hardcoded for that reason...
    
    Now the question is if that is a LDFLAG at all and if we want
    to just remove that use and everything works fine?
    
    CC: @lattera

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -8,5 +8,5 @@ SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 # Overridable as a user may want to use -fstack-protector
 SSP_CFLAGS?=	-fstack-protector-all
 CFLAGS+=	${SSP_CFLAGS}
-LDFLAGS+=	${SSP_CFLAGS}
+#LDFLAGS+=	${SSP_CFLAGS}
 .endif
