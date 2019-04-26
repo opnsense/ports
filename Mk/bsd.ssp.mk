492285be94d1c0c2359d30a23f2daf96cdf0625b
Franco Fichtner <franco@opnsense.org>
2019-04-26 09:44:22 +0200

    Framework: compat with old SSP_CLFAGS behaviour
    Keep behaviour the way it was.  misc/mc-light configure fails with
    -fstack-protector-all, but not without SSP, -fstack-protector
    and -fstack-protector-strong.
    
    We should move to -fstack-protector-strong if only for follwing
    FreeBSD ports, but since the impact on the ports is hard to
    follow in OPNsense and would creep into package updates of stable
    packages in 19.1.x it's better to wait for the clean reinstall of
    19.7 packages during the upgrade.
    
    CC: @lattera

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -8,5 +8,10 @@ SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 # Overridable as a user may want to use -fstack-protector
 SSP_CFLAGS?=	-fstack-protector-all
 CFLAGS+=	${SSP_CFLAGS}
-#LDFLAGS+=	${SSP_CFLAGS}
+LDFLAGS+=	${SSP_CFLAGS}
+.if ${SSP_CFLAGS:M-fstack-protector-all}
+LDFLAGS+=	-fstack-protector
+.else
+LDFLAGS+=	${SSP_CFLAGS}
+.endif
 .endif
