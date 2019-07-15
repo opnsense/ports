5c5b464d662bb540576bebe4b500d5538a174558
Franco Fichtner <franco@opnsense.org>
2019-07-15 12:24:18 +0200

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -8,9 +8,5 @@ SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 # Overridable as a user may want to use -fstack-protector
 SSP_CFLAGS?=	-fstack-protector-all
 CFLAGS+=	${SSP_CFLAGS}
-.if ${SSP_CFLAGS:M-fstack-protector-all}
-LDFLAGS+=	-fstack-protector
-.else
 LDFLAGS+=	${SSP_CFLAGS}
-.endif
 .endif
