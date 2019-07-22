5496d93d25be0a75106e7be5e1a5a2522fc35c4f
Franco Fichtner <franco@opnsense.org>
2019-07-22 19:52:17 +0200

    Framework: partially sync with upstream
    Taken from: HardenedBSD

--- Mk/Uses/cabal.mk
+++ Mk/Uses/cabal.mk
@@ -107,13 +107,14 @@ cabal-extract-deps:
 # Generates USE_CABAL= ... line ready to be pasted into the port based on artifacts of cabal-extract-deps.
 make-use-cabal:
 	@echo ====================
-	@find ${CABAL_HOME} -name '*.conf' -exec basename {} + | sed -E 's|-[0-9a-z]{64}\.conf||' | sort | xargs echo -n USE_CABAL= && echo
+	@echo -n USE_CABAL=
+	@find ${CABAL_HOME} -name '*.conf' -exec basename {} + | sed -E 's|-[0-9a-z]{64}\.conf||' | sort | sed 's/$$/ \\/'
 
-# Checks USE_CABAL items that have revisions.
-check-revs:
+# Re-generates USE_CABAL items to have revision numbers.
+make-use-cabal-revs:
 .  for package in ${_use_cabal}
-	@(fetch -o /dev/null http://hackage.haskell.org/package/${package:C/_[0-9]+//}/revision/1.cabal 2>/dev/null && echo "Package ${package} has revisions") || true
-	@([ -d ${DISTDIR}/${DIST_SUBDIR}/${package:C/_[0-9]+//}/revision ] && echo "    hint: " `find ${DISTDIR}/${DIST_SUBDIR}/${package:C/_[0-9]+//} -name *.cabal | xargs basename`) || true
+	@(${ENV} HTTP_ACCEPT="application/json" fetch -q -o - http://hackage.haskell.org/package/${package:C/_[0-9]+//}/revisions/ | sed -Ee 's/.*":([0-9]+)}\]/${package:C/_[0-9]+//}_\1 /' -e 's/_0//')
+	@echo '\'
 .  endfor
 
 .  if !defined(CABAL_BOOTSTRAP)
