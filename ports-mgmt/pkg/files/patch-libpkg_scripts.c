b2b6407d0eac5f942127ac91243a595ae0d2fe9b
Franco Fichtner <franco@opnsense.org>
2019-09-23 06:16:43 +0200

    ports-mgmt/pkg: partially sync with upstream
    Taken from: HardenedBSD

--- ports-mgmt/pkg/files/patch-libpkg_scripts.c
+++ ports-mgmt/pkg/files/patch-libpkg_scripts.c
@@ -1,6 +1,6 @@
---- libpkg/scripts.c.orig	2016-07-05 15:06:13 UTC
+--- libpkg/scripts.c.orig	2019-09-18 07:11:10 UTC
 +++ libpkg/scripts.c
-@@ -65,7 +65,7 @@ pkg_script_run(struct pkg * const pkg, p
+@@ -68,7 +68,7 @@ pkg_script_run(struct pkg * const pkg, p
  	ssize_t bytes_written;
  	size_t script_cmd_len;
  	long argmax;
@@ -9,7 +9,7 @@
  	bool do_reap;
  	pid_t mypid;
  	struct procctl_reaper_status info;
-@@ -98,7 +98,7 @@ pkg_script_run(struct pkg * const pkg, p
+@@ -103,7 +103,7 @@ pkg_script_run(struct pkg * const pkg, p
  
  	assert(i < sizeof(map) / sizeof(map[0]));
  
@@ -18,15 +18,15 @@
  	mypid = getpid();
  	do_reap = procctl(P_PID, mypid, PROC_REAP_ACQUIRE, NULL) == 0;
  #endif
-@@ -199,7 +199,6 @@ pkg_script_run(struct pkg * const pkg, p
+@@ -218,7 +218,6 @@ pkg_script_run(struct pkg * const pkg, p
+ 					exit(0);
  
- 			if (WEXITSTATUS(pstat) != 0) {
  				pkg_emit_error("%s script failed", map[i].arg);
 -				ret = EPKG_FATAL;
  				goto cleanup;
  			}
  		}
-@@ -213,7 +212,7 @@ cleanup:
+@@ -232,7 +231,7 @@ cleanup:
  	if (stdin_pipe[1] != -1)
  		close(stdin_pipe[1]);
  
--- ports-mgmt/pkg/files/patch-libpkg_utils.c
+++ ports-mgmt/pkg/files/patch-libpkg_utils.c
@@ -1,6 +1,6 @@
---- libpkg/utils.c.orig	2018-03-04 08:35:35 UTC
+--- libpkg/utils.c.orig	2019-09-18 07:11:10 UTC
 +++ libpkg/utils.c
-@@ -349,7 +349,7 @@ is_valid_abi(const char *arch, bool emit
+@@ -350,7 +350,7 @@ is_valid_abi(const char *arch, bool emit
  bool
  is_valid_os_version(struct pkg *pkg)
  {
