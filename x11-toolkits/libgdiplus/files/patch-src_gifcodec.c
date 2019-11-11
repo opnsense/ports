a0dcdbf69beec01d390dfed4bc63c466be5a4f98
Franco Fichtner <franco@opnsense.org>
2019-11-11 09:52:42 +0100

    */*: sync with upstream
    Taken from: HardenedBSD

--- x11-toolkits/libgdiplus/files/patch-src_gifcodec.c
+++ x11-toolkits/libgdiplus/files/patch-src_gifcodec.c
@@ -1,10 +1,9 @@
---- src/libimage/gif.c.orig	2006-03-25 22:50:51 UTC
-+++ src/libimage/gif.c
-@@ -28,6 +28,314 @@
-   distribution. 
- */
+--- src/gifcodec.c.orig	2018-03-28 19:27:53 UTC
++++ src/gifcodec.c
+@@ -39,6 +39,292 @@ GUID gdip_gif_image_format_guid = {0xb96
  
-+#define ABS(x) ((x) > 0 ? (x) : (-(x)))
+ #include "gifcodec.h"
+ 
 +#define COLOR_ARRAY_SIZE 32768
 +#define BITS_PER_PRIM_COLOR 5
 +#define MAX_PRIM_COLOR      0x1f
@@ -291,197 +290,18 @@
 +
 +    return GIF_OK;
 +}
-+ 
-+static void
-+#if GIFLIB_MAJOR >= 5
-+localPrintGifError(int ErrorCode)
-+#else
-+localPrintGifError(void)
-+#endif
-+{
-+#if GIFLIB_MAJOR >= 5
-+    char *Err = GifErrorString(ErrorCode);
-+#else
-+    char *Err = GifErrorString();
-+    int ErrorCode = GifError();
-+#endif
-+
-+    if (Err != NULL)
-+        fprintf(stderr, "\nGIF-LIB error: %s.\n", Err);
-+    else
-+        fprintf(stderr, "\nGIF-LIB undefined error %d.\n", ErrorCode);
-+}
-+
- int
- read_gif(const char *filename, int *width, int *height, unsigned char **rgb)
- {
-@@ -42,11 +350,15 @@ read_gif(const char *filename, int *widt
-     int color_index;
-     unsigned char *ptr = NULL;
  
-+#if GIFLIB_MAJOR >= 5
-+    infile = DGifOpenFileName(filename, NULL);
-+#else
-     infile = DGifOpenFileName(filename);
-+#endif
- 
-     if (infile == NULL)
-     {
--        PrintGifError();
-+        fprintf(stderr, "Can't open GIF file %s\n", filename);
-         return(0);
-     }
- 
-@@ -54,7 +366,11 @@ read_gif(const char *filename, int *widt
-     {
-         if (DGifGetRecordType(infile, &record_type) == GIF_ERROR) 
-         {
--            PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+            localPrintGifError(infile->Error);
-+#else
-+            localPrintGifError();
-+#endif
-             return(0);
-         }
- 
-@@ -63,7 +379,11 @@ read_gif(const char *filename, int *widt
-         case IMAGE_DESC_RECORD_TYPE:
-             if (DGifGetImageDesc(infile) == GIF_ERROR)
-             {
--                PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+                localPrintGifError(infile->Error);
-+#else
-+                localPrintGifError();
-+#endif
-                 return(0);
-             }
- 
-@@ -107,14 +427,22 @@ read_gif(const char *filename, int *widt
-             GifByteType *ext;
-             if (DGifGetExtension(infile, &ext_code, &ext) == GIF_ERROR) 
-             {
--                PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+                localPrintGifError(infile->Error);
-+#else
-+                localPrintGifError();
-+#endif
-                 return(0);
-             }
-             while (ext != NULL) 
-             {
-                 if (DGifGetExtensionNext(infile, &ext) == GIF_ERROR) 
-                 {
--                    PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+                    localPrintGifError(infile->Error);
-+#else
-+                    localPrintGifError();
-+#endif
-                     return(0);
-                 }
-             }
-@@ -154,7 +482,11 @@ read_gif(const char *filename, int *widt
-     
-     free(buffer);
- 
-+#if GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1 || GIFLIB_MAJOR > 5
-+    DGifCloseFile(infile, NULL);
-+#else
-     DGifCloseFile(infile);
-+#endif
-     return(1);
- }
- 
-@@ -178,7 +510,11 @@ write_gif(const char *filename, int widt
-         return(0);
-     }
- 
-+#if GIFLIB_MAJOR >= 5
-+    colormap = GifMakeMapObject(colormap_size, NULL);
-+#else
-     colormap = MakeMapObject(colormap_size, NULL);
-+#endif
- 
-     for (i = 0; i < width * height; i++)
-     {
-@@ -190,7 +526,7 @@ write_gif(const char *filename, int widt
-     if (QuantizeBuffer(width, height, &colormap_size, red, green, blue,   
-                        buffer, colormap->Colors) == GIF_ERROR)
-     {
--        PrintGifError();
-+        fprintf(stderr, "Can't quantize buffer\n");
-         return(0);
-     }
- 
-@@ -198,24 +534,36 @@ write_gif(const char *filename, int widt
-     free(green);
-     free(blue);
- 
--    outfile = EGifOpenFileName((char *) filename, FALSE);
-+#if GIFLIB_MAJOR >= 5
-+    outfile = EGifOpenFileName((char *) filename, false, NULL);
-+#else
-+    outfile = EGifOpenFileName((char *) filename, false);
-+#endif
-     if (outfile == NULL)
-     {
--        PrintGifError();
-+        fprintf(stderr, "Can't open GIF file %s\n", filename);
-         return(0);
-     }
- 
-     if (EGifPutScreenDesc(outfile, width, height, colormap_size, 0, colormap)
-         == GIF_ERROR)
-     {
--        PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+        localPrintGifError(outfile->Error);
-+#else
-+        localPrintGifError();
-+#endif
-         return(0);
-     }
- 
--    if (EGifPutImageDesc(outfile, 0, 0, width, height, FALSE, NULL)
-+    if (EGifPutImageDesc(outfile, 0, 0, width, height, false, NULL)
-         == GIF_ERROR)
-     {
--        PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+        localPrintGifError(outfile->Error);
-+#else
-+        localPrintGifError();
-+#endif
-         return(0);
-     }
- 
-@@ -224,7 +572,11 @@ write_gif(const char *filename, int widt
-     {
-         if (EGifPutLine(outfile, ptr, width) == GIF_ERROR)
-         {
--            PrintGifError();
-+#if GIFLIB_MAJOR >= 5
-+            localPrintGifError(outfile->Error);
-+#else
-+            localPrintGifError();
-+#endif
-             return(0);
-         }
-         ptr += width;
-@@ -232,8 +584,12 @@ write_gif(const char *filename, int widt
- 
-     EGifSpew(outfile);
- 
-+#if GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1 || GIFLIB_MAJOR > 5
-+    if (EGifCloseFile(outfile, NULL) == GIF_ERROR) 
-+#else
-     if (EGifCloseFile(outfile) == GIF_ERROR) 
--        PrintGifError();
-+#endif
-+        fprintf(stderr, "Can't close GIF file %s\n", filename);
- 
-     free(buffer);
- 
+ /* Data structure used for callback */
+ typedef struct
+@@ -852,11 +1138,7 @@ gdip_save_gif_image (void *stream, GpIma
+ 					}
+ 				}
+ 				if (
+-#if GIFLIB_MAJOR >= 5
+-				GifQuantizeBuffer(
+-#else
+ 				QuantizeBuffer(
+-#endif
+ 						bitmap_data->width, bitmap_data->height, &cmap_size, 
+ 						red,  green, blue, pixbuf, cmap->Colors) == GIF_ERROR) {
+ 					goto error;
