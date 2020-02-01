e239f6c6667afe6e2ae81abcbaba97aa321b0db7
Franco Fichtner <franco@opnsense.org>
2020-02-01 15:26:57 +0100

    Framework: sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -547,7 +547,9 @@ GL_COMMIT+=	${GL_TUPLE:C@^(([^:]*://[^:/]*(:[0-9]{1,5})?(/[^:]*[^/])?:)?)([^:]*)
 GL_SUBDIR+=	${GL_TUPLE:C@^(([^:]*://[^:/]*(:[0-9]{1,5})?(/[^:]*[^/])?:)?)([^:]*):([^:]*):([^:]*)(:[^:/]*)((/.*)?)@\9\8@:M/*:S/^\///}
 .  endif
 
-.  if empty(USE_GITLAB:Mnodefault)
+.  if !empty(USE_GITLAB:Mgitea)
+MASTER_SITES+=	${GL_SITE}/${GL_ACCOUNT}/${GL_PROJECT}/archive/${GL_COMMIT}.tar.gz?dummy=/
+.  elif empty(USE_GITLAB:Mnodefault)
 MASTER_SITES+=	${GL_SITE}/${GL_ACCOUNT}/${GL_PROJECT}/repository/${GL_COMMIT}/archive.tar.gz?dummy=/
 .  endif
 GL_SITE_DEFAULT=	https://gitlab.com
