83935c0ce6b41b585e5613bcc23c68bf3f8e7a2f
Franco Fichtner <franco@opnsense.org>
2020-03-16 06:48:42 +0100

    opnsense/opnsense-update-devel: new temporary port

---
+++ opnsense/opnsense-update-devel/files/patch-update_opnsense-update.sh
@@ -0,0 +1,11 @@
+--- update/opnsense-update.sh.orig	2020-03-16 05:47:51 UTC
++++ update/opnsense-update.sh
+@@ -42,7 +42,7 @@ KERNELDIR="/boot/kernel"
+ TEE="/usr/bin/tee -a"
+ PRODUCT="OPNsense"
+ PKG="pkg-static"
+-VERSION="20.1.2"
++VERSION="20.7.a"
+ 
+ ORIGIN="/usr/local/etc/pkg/repos/${PRODUCT}.conf"
+ PENDINGDIR="${WORKPREFIX}/.sets.pending"
