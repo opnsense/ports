5ed3b932d19a111654770a59d4de98de696bcd9f
Franco Fichtner <franco@opnsense.org>
2020-09-04 03:51:40 +0200

    Framework: partially sync with upstream
    Taken from: HardenedBSD

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -54,7 +54,7 @@ _KDE_RELNAME=		KDE${_KDE_VERSION}
 
 # === VERSIONS OF THE DIFFERENT COMPONENTS =====================================
 # Current KDE desktop.
-KDE_PLASMA_VERSION?=		5.19.4
+KDE_PLASMA_VERSION?=		5.19.5
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
@@ -62,8 +62,8 @@ KDE_FRAMEWORKS_VERSION?=	5.73.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
-KDE_APPLICATIONS_VERSION?=	20.08.0
-KDE_APPLICATIONS_SHLIB_VER?=	5.15.0
+KDE_APPLICATIONS_VERSION?=	20.08.1
+KDE_APPLICATIONS_SHLIB_VER?=	5.15.1
 KDE_APPLICATIONS_BRANCH?=	stable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
