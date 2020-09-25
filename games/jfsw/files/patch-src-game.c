eb6042d4fca62e955f4fb6fc9b2ad7e1c504bc72
Franco Fichtner <franco@opnsense.org>
2020-09-25 15:04:32 +0200

    */*: sync with upstream
    Taken from: HardenedBSD

--- games/jfsw/files/patch-src-game.c
+++ games/jfsw/files/patch-src-game.c
@@ -1,8 +1,9 @@
-Rename PREFIX to DATADIR as its purpose is only to find data files.
+Rename PREFIX to DATADIR as its only purpose is to access data files
+(and avoid conflict with FreeBSD PREFIX, which has another meaning)
 
---- src/game.c.orig	2020-09-03 06:19:21 UTC
+--- src/game.c.orig	2020-09-22 22:08:35 UTC
 +++ src/game.c
-@@ -3470,9 +3470,9 @@ int app_main(int argc, char const * const argv[])
+@@ -3405,9 +3405,9 @@ int app_main(int argc, char const * const argv[])
      }
  #endif
  
