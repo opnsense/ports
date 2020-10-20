d83977b8fef568ff74ba686b8a0984dc17c97dd1
Franco Fichtner <franco@opnsense.org>
2020-10-20 20:49:39 +0200

    */*: sync with upstream
    Taken from: HardenedBSD

--- biology/hyphy/files/patch-src_core_matrix.cpp
+++ biology/hyphy/files/patch-src_core_matrix.cpp
@@ -1,20 +1,11 @@
---- src/core/matrix.cpp.orig	2020-09-02 16:12:43 UTC
+--- src/core/matrix.cpp.orig	2020-10-16 23:15:05 UTC
 +++ src/core/matrix.cpp
-@@ -3128,8 +3128,6 @@ void    _Matrix::AddMatrix  (_Matrix& storage, _Matrix
-                      CELL_OP (idx+8);
-                      CELL_OP (idx+12);
-                  }
--            }
--
-  #else
-                 for (long idx = 0; idx < upto; idx+=4) {
-                     stData[idx]+=argData[idx];
-@@ -3138,7 +3136,7 @@ void    _Matrix::AddMatrix  (_Matrix& storage, _Matrix
-                     stData[idx+3]+=argData[idx+3];
-                 }
- #endif
--                
-+            }
-             if (subtract)
-                 for (long idx = upto; idx < secondArg.lDim; idx++) {
-                     stData[idx]-=argData[idx];
+@@ -2236,7 +2236,7 @@ bool    _Matrix::IncreaseStorage    (void) {
+         } else {
+             theData = (hyFloat*) MemReallocate(theData, lDim*sizeof(void*));
+             for (long i = lDim-allocationBlock; i < lDim; i++) {
+-                theData [i] = ZEROPOINTER;
++                theData [i] = ZEROOBJECT;
+             }
+         }
+     } else {
