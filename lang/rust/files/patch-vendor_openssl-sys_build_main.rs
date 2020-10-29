35676b0824a9c903c99abdae28e81d398a145cd4
Franco Fichtner <franco@opnsense.org>
2020-10-29 11:03:00 +0100

    lang/rust: sync with upstream
    Taken from: HardenedBSD

--- lang/rust/files/patch-vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-vendor_openssl-sys_build_main.rs
@@ -1,21 +1,19 @@
---- vendor/openssl-sys/build/main.rs.orig	2020-06-01 17:45:25 UTC
-+++ vendor/openssl-sys/build/main.rs
-@@ -204,6 +204,9 @@ See rust-openssl README for more information:
-             (3, 0, 0) => ('3', '0', '0'),
-             (3, 0, 1) => ('3', '0', '1'),
-             (3, 0, _) => ('3', '0', 'x'),
-+            (3, 1, 0) => ('3', '1', '0'),
-+            (3, 1, _) => ('3', '1', 'x'),
-+            (3, 2, 0) => ('3', '2', '0'),
+--- vendor/openssl-sys/build/main.rs.ORIG	2020-10-07 02:33:31.000000000 -0700
++++ vendor/openssl-sys/build/main.rs	2020-10-23 20:45:37.047602000 -0700
+@@ -221,6 +221,7 @@
+             (3, 1, 0) => ('3', '1', '0'),
+             (3, 1, _) => ('3', '1', 'x'),
+             (3, 2, 0) => ('3', '2', '0'),
++            (3, 2, 2) => ('3', '2', '2'),
              _ => version_error(),
          };
  
-@@ -244,7 +247,7 @@ fn version_error() -> ! {
+@@ -261,7 +262,7 @@
          "
  
  This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
--through 3.0.x, but a different version of OpenSSL was found. The build is now aborting
-+through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
+-through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
++through 3.2.2, but a different version of OpenSSL was found. The build is now aborting
  due to this version mismatch.
  
  "
