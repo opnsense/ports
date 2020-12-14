f7daad9835cbe25aa3b23dd24f9541a7b4f1d7f9
Franco Fichtner <franco@opnsense.org>
2020-12-14 14:51:46 +0100

    */*: sync with upstream
    Taken from: HardenedBSD

--- misc/openvdb/files/patch-openvdb_openvdb_points_AttributeSet.h
+++ misc/openvdb/files/patch-openvdb_openvdb_points_AttributeSet.h
@@ -1,5 +1,5 @@
---- openvdb/points/AttributeSet.h.orig	2020-11-27 13:35:57 UTC
-+++ openvdb/points/AttributeSet.h
+--- openvdb/openvdb/points/AttributeSet.h.orig	2020-12-14 01:40:20 UTC
++++ openvdb/openvdb/points/AttributeSet.h
 @@ -14,6 +14,7 @@
  #include <openvdb/version.h>
  #include <openvdb/MetaMap.h>
