b9c54614114c3a9b6fc3dd7f74b21abaeae82116
Franco Fichtner <franco@opnsense.org>
2021-02-16 09:03:25 +0100

    security/libressl: restore patch of course

---
+++ security/libressl/files/patch-include_openssl_opensslv.h
@@ -0,0 +1,14 @@
+--- include/openssl/opensslv.h.orig	2021-02-03 12:22:46 UTC
++++ include/openssl/opensslv.h
+@@ -8,7 +8,11 @@
+ #define LIBRESSL_VERSION_TEXT   "LibreSSL 3.2.4"
+ 
+ /* These will never change */
++#ifndef OPENSSL_IS_LIBRESSL
+ #define OPENSSL_VERSION_NUMBER	0x20000000L
++#else
++#define OPENSSL_VERSION_NUMBER	0x1000107fL
++#endif /* OPENSSL_IS_LIBRESSL */
+ #define OPENSSL_VERSION_TEXT	LIBRESSL_VERSION_TEXT
+ #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
+ 
