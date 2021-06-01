1a5af94bb5503f3e8b29626b8d3c05e801586bc9
Franco Fichtner <franco@opnsense.org>
2021-06-01 14:07:18 +0200

    Framework: partially sync with upstream
    Taken from: HardenedBSD

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -4165,6 +4165,7 @@ _FLAVOR_RECURSIVE_SH= \
 	for dir in $${recursive_dirs}; do \
 		unset flavor; \
 		case $${dir} in \
+			*@*/*) ;; \
 			*@*) \
 				flavor=$${dir\#*@}; \
 				dir=$${dir%@*}; \
@@ -4174,7 +4175,7 @@ _FLAVOR_RECURSIVE_SH= \
 		/*) ;; \
 		*) dir=${PORTSDIR}/$$dir ;; \
 		esac; \
-		(cd $$dir; ${SETENV} FLAVOR=$${flavor} ${MAKE} $${recursive_cmd}); \
+		(cd $$dir; ${SETENV} $${flavor:+FLAVOR=$${flavor}} ${MAKE} $${recursive_cmd}); \
 	done
 
 # This script is shared among several dependency list variables.  See file for
