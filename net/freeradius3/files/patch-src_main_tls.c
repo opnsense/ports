208b6e2fcbaa7a33ec7c87fd53c1bbe29c84cb70
Franco Fichtner <franco@opnsense.org>
2021-10-11 08:34:08 +0200

    net/freeradius3: partially sync with upstream
    Taken from: FreeBSD

--- net/freeradius3/files/patch-src_main_tls.c
+++ net/freeradius3/files/patch-src_main_tls.c
@@ -1,6 +1,6 @@
---- src/main/tls.c.orig	2021-06-09 07:14:06 UTC
+--- src/main/tls.c.orig	2021-10-07 19:46:04 UTC
 +++ src/main/tls.c
-@@ -1580,7 +1580,7 @@ static int load_dh_params(SSL_CTX *ctx, char *file)
+@@ -1697,7 +1697,7 @@ static int load_dh_params(SSL_CTX *ctx, char *file)
  	 *
  	 * Change suggested by @t8m
  	 */
@@ -9,7 +9,16 @@
  	if (FIPS_mode() > 0) {
  		WARN(LOG_PREFIX ": Ignoring user-selected DH parameters in FIPS mode. Using defaults.");
  		return 0;
-@@ -3987,7 +3987,7 @@ post_ca:
+@@ -3209,7 +3209,7 @@ int cbtls_verify(int ok, X509_STORE_CTX *ctx)
+ 
+ 		rad_assert(ssn != NULL);
+ 
+-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
++#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+ 		/*
+ 		 *	See if there are any untrusted certificates.
+ 		 *	If so, complain about them.
+@@ -4169,7 +4169,7 @@ post_ca:
  	 *	disable early data.
  	 *
  	 */
