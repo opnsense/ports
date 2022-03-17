9c994af1bfb22c9f79ad8e0e25e27b7c8dbc0c0e
Franco Fichtner <franco@opnsense.org>
2022-03-17 18:26:40 +0100

    security/libressl: bump to 3.3.6

--- security/libressl/files/patch-include_openssl_opensslv.h
+++ security/libressl/files/patch-include_openssl_opensslv.h
@@ -1,7 +1,7 @@
---- include/openssl/opensslv.h.orig	2021-09-30 14:33:48 UTC
+--- include/openssl/opensslv.h.orig	2022-03-13 17:36:37 UTC
 +++ include/openssl/opensslv.h
 @@ -8,7 +8,11 @@
- #define LIBRESSL_VERSION_TEXT   "LibreSSL 3.3.5"
+ #define LIBRESSL_VERSION_TEXT   "LibreSSL 3.3.6"
  
  /* These will never change */
 +#ifndef OPENSSL_IS_LIBRESSL
