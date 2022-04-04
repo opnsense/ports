be89a81ed839103d698e937244e02060379c6eaa
Franco Fichtner <franco@opnsense.org>
2022-04-04 16:09:52 +0200

    */*: sync with upstream
    Taken from: FreeBSD

---
+++ www/iridium/files/patch-net_socket_udp__socket__posix.cc
@@ -0,0 +1,11 @@
+--- net/socket/udp_socket_posix.cc.orig	2022-04-01 07:48:30 UTC
++++ net/socket/udp_socket_posix.cc
+@@ -574,7 +574,7 @@ int UDPSocketPosix::SetDoNotFragment() {
+ }
+ 
+ void UDPSocketPosix::SetMsgConfirm(bool confirm) {
+-#if !BUILDFLAG(IS_APPLE)
++#if !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_BSD)
+   if (confirm) {
+     sendto_flags_ |= MSG_CONFIRM;
+   } else {
