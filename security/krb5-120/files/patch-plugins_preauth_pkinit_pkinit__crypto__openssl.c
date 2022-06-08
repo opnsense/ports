6ed4af5bf9db95684c1db7f353d47b6ef6d01814
Franco Fichtner <franco@opnsense.org>
2022-06-08 08:53:13 +0200

    security/krb5-120: fix LibreSSL builds for now

--- security/krb5-120/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
+++ security/krb5-120/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
@@ -1,6 +1,6 @@
---- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2021-11-05 16:24:07.000000000 -0700
-+++ plugins/preauth/pkinit/pkinit_crypto_openssl.c	2021-11-08 10:10:45.431325000 -0800
-@@ -178,7 +178,8 @@
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2022-05-26 01:35:20 UTC
++++ plugins/preauth/pkinit/pkinit_crypto_openssl.c
+@@ -184,7 +184,8 @@ pkcs11err(int err);
      (*_x509_pp) = PKCS7_cert_from_signer_info(_p7,_si)
  #endif
  
@@ -10,8 +10,35 @@
  
  /* 1.1 standardizes constructor and destructor names, renaming
   * EVP_MD_CTX_{create,destroy} and deprecating ASN1_STRING_data. */
-@@ -722,6 +723,10 @@
-     DH_free(dh);
+@@ -320,7 +321,7 @@ decode_bn_der(const uint8_t *der, size_t len)
+     return bn;
+ }
+ 
+-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
++#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+ static int
+ params_valid(EVP_PKEY *params)
+ {
+@@ -346,7 +347,7 @@ params_valid(EVP_PKEY *params)
+ }
+ #endif
+ 
+-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
++#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+ 
+ #if OPENSSL_VERSION_NUMBER >= 0x30000000L
+ static EVP_PKEY *
+@@ -588,7 +589,7 @@ set_padded_derivation(EVP_PKEY_CTX *ctx)
+ {
+     EVP_PKEY_CTX_set_dh_pad(ctx, 1);
+ }
+-#elif OPENSSL_VERSION_NUMBER >= 0x10100000L
++#elif OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+ static void
+ set_padded_derivation(EVP_PKEY_CTX *ctx)
+ {
+@@ -731,6 +732,10 @@ cleanup:
+     EVP_PKEY_CTX_free(ctx);
      return pkey;
  }
 +
@@ -19,5 +46,5 @@
 +#define static_ASN1_SEQUENCE_END_name	ASN1_SEQUENCE_END_name
 +#endif
  
- static struct pkcs11_errstrings {
-     short code;
+ #if OPENSSL_VERSION_NUMBER >= 0x30000000L
+ 
