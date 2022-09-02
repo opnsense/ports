206019407da2934c6255ecf496731026a7856adb
Franco Fichtner <franco@opnsense.org>
2022-09-02 10:19:39 +0200

    sysutils/syslog-ng: partially sync with upstream
    Taken from: FreeBSD

--- sysutils/syslog-ng/files/patch-lib_tlscontext.c
+++ sysutils/syslog-ng/files/patch-lib_tlscontext.c
@@ -1,19 +1,6 @@
---- lib/tlscontext.c.orig	2022-06-03 13:40:39 UTC
+--- lib/tlscontext.c.orig	2022-08-28 11:50:04 UTC
 +++ lib/tlscontext.c
-@@ -598,10 +598,12 @@ tls_context_setup_cipher_suite(TLSContext *self)
- static gboolean
- tls_context_setup_sigalgs(TLSContext *self)
- {
-+#if !defined(LIBRESSL_VERSION_NUMBER)
-   if (self->sigalgs && !SSL_CTX_set1_sigalgs_list(self->ssl_ctx, self->sigalgs))
-     return FALSE;
- 
-   if (self->client_sigalgs && !SSL_CTX_set1_client_sigalgs_list(self->ssl_ctx, self->client_sigalgs))
-+#endif
-     return FALSE;
- 
-   return TRUE;
-@@ -1025,7 +1027,7 @@ tls_context_set_key_file(TLSContext *self, const gchar
+@@ -1029,7 +1029,7 @@ tls_context_set_key_file(TLSContext *self, const gchar
  gboolean
  tls_context_set_keylog_file(TLSContext *self, gchar *keylog_file_path, GError **error)
  {
