ca27f20909d4581d3a918efb027bdbbc6c7efab9
Franco Fichtner <franco@opnsense.org>
2023-01-20 21:52:34 +0100

    security/krb5-120: sync with upstream
    Taken from: FreeBSD

--- security/krb5-120/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
+++ security/krb5-120/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
@@ -1,34 +1,38 @@
---- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2022-05-26 01:35:20 UTC
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2022-10-17 09:52:43 UTC
 +++ plugins/preauth/pkinit/pkinit_crypto_openssl.c
-@@ -184,7 +184,8 @@ pkcs11err(int err);
+@@ -184,6 +184,17 @@ pkcs11err(int err);
      (*_x509_pp) = PKCS7_cert_from_signer_info(_p7,_si)
  #endif
  
--#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if (defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L) || \
-+     defined(LIBRESSL_VERSION_NUMBER)
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
++
++/*
++ * 1.1 adds DHX support, which uses the RFC 3279 DomainParameters encoding we
++ * need for PKINIT.  For 1.0 we must use the original DH type when creating
++ * EVP_PKEY objects.
++ */
++#define EVP_PKEY_DHX EVP_PKEY_DH
++#define d2i_DHxparams d2i_DHparams
++#endif
++
+ #if OPENSSL_VERSION_NUMBER < 0x10100000L
  
  /* 1.1 standardizes constructor and destructor names, renaming
-  * EVP_MD_CTX_{create,destroy} and deprecating ASN1_STRING_data. */
-@@ -320,7 +321,7 @@ decode_bn_der(const uint8_t *der, size_t len)
-     return bn;
- }
+@@ -193,13 +204,6 @@ pkcs11err(int err);
+ #define EVP_MD_CTX_free EVP_MD_CTX_destroy
+ #define ASN1_STRING_get0_data ASN1_STRING_data
  
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
- static int
- params_valid(EVP_PKEY *params)
- {
-@@ -346,7 +347,7 @@ params_valid(EVP_PKEY *params)
- }
- #endif
+-/*
+- * 1.1 adds DHX support, which uses the RFC 3279 DomainParameters encoding we
+- * need for PKINIT.  For 1.0 we must use the original DH type when creating
+- * EVP_PKEY objects.
+- */
+-#define EVP_PKEY_DHX EVP_PKEY_DH
+-
+ /* 1.1 makes many handle types opaque and adds accessors.  Add compatibility
+  * versions of the new accessors we use for pre-1.1. */
  
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
- 
- #if OPENSSL_VERSION_NUMBER >= 0x30000000L
- static EVP_PKEY *
-@@ -588,7 +589,7 @@ set_padded_derivation(EVP_PKEY_CTX *ctx)
+@@ -588,7 +592,7 @@ set_padded_derivation(EVP_PKEY_CTX *ctx)
  {
      EVP_PKEY_CTX_set_dh_pad(ctx, 1);
  }
@@ -37,14 +41,3 @@
  static void
  set_padded_derivation(EVP_PKEY_CTX *ctx)
  {
-@@ -731,6 +732,10 @@ cleanup:
-     EVP_PKEY_CTX_free(ctx);
-     return pkey;
- }
-+
-+#if defined(LIBRESSL_VERSION_NUMBER) && !defined(static_ASN1_SEQUENCE_END_name)
-+#define static_ASN1_SEQUENCE_END_name	ASN1_SEQUENCE_END_name
-+#endif
- 
- #if OPENSSL_VERSION_NUMBER >= 0x30000000L
- 
