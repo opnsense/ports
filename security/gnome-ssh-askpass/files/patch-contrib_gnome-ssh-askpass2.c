3e02d2b4f14f74390231dc507979800b82d9ef13
Franco Fichtner <franco@opnsense.org>
2024-04-15 19:12:18 +0200

    */*: sync with upstream
    Taken from: FreeBSD

--- security/gnome-ssh-askpass/files/patch-contrib_gnome-ssh-askpass2.c
+++ security/gnome-ssh-askpass/files/patch-contrib_gnome-ssh-askpass2.c
@@ -1,53 +1,57 @@
---- contrib/gnome-ssh-askpass2.c.orig	2009-11-18 06:51:59 UTC
+--- contrib/gnome-ssh-askpass2.c.orig	2024-03-11 05:20:49 UTC
 +++ contrib/gnome-ssh-askpass2.c
-@@ -88,12 +88,13 @@ passphrase_dialog(char *message)
+@@ -151,7 +151,7 @@ passphrase_dialog(char *message, int prompt_type)
  {
  	const char *failed;
  	char *passphrase, *local;
 -	int result, grab_tries, grab_server, grab_pointer;
 +	int result, grab_tries, grab_server, grab_pointer, grab_keyboard;
- 	GtkWidget *dialog, *entry;
+ 	int buttons, default_response;
+ 	GtkWidget *parent_window, *dialog, *entry;
  	GdkGrabStatus status;
+@@ -160,6 +160,7 @@ passphrase_dialog(char *message, int prompt_type)
  
  	grab_server = (getenv("GNOME_SSH_ASKPASS_GRAB_SERVER") != NULL);
  	grab_pointer = (getenv("GNOME_SSH_ASKPASS_GRAB_POINTER") != NULL);
 +	grab_keyboard = (getenv("GNOME_SSH_ASKPASS_GRAB_KEYBOARD") != NULL);
  	grab_tries = 0;
  
- 	dialog = gtk_message_dialog_new(NULL, 0,
-@@ -138,17 +139,21 @@ passphrase_dialog(char *message)
+ 	fg_set = parse_env_hex_color("GNOME_SSH_ASKPASS_FG_COLOR", &fg);
+@@ -243,18 +244,20 @@ passphrase_dialog(char *message, int prompt_type)
  			}
  		}
  	}
 -	for(;;) {
--		status = gdk_keyboard_grab((GTK_WIDGET(dialog))->window,
--		   FALSE, GDK_CURRENT_TIME);
+-		status = gdk_keyboard_grab(
+-		    gtk_widget_get_window(GTK_WIDGET(dialog)), FALSE,
+-		    GDK_CURRENT_TIME);
 -		if (status == GDK_GRAB_SUCCESS)
 -			break;
 -		usleep(GRAB_WAIT * 1000);
 -		if (++grab_tries > GRAB_TRIES) {
 -			failed = "keyboard";
 -			goto nograbkb;
-+
-+	if (grab_keyboard) {
-+		for(;;) {
-+			status = gdk_keyboard_grab((GTK_WIDGET(dialog))->window,
-+			   FALSE, GDK_CURRENT_TIME);
-+			if (status == GDK_GRAB_SUCCESS)
-+				break;
-+			usleep(GRAB_WAIT * 1000);
-+			if (++grab_tries > GRAB_TRIES) {
-+				failed = "keyboard";
-+				goto nograbkb;
-+			}
- 		}
- 	}
-+
+-		}
+-	}
++    if (grab_keyboard) {
++	    for(;;) {
++		    status = gdk_keyboard_grab(
++		        gtk_widget_get_window(GTK_WIDGET(dialog)), FALSE,
++		        GDK_CURRENT_TIME);
++		    if (status == GDK_GRAB_SUCCESS)
++			    break;
++		    usleep(GRAB_WAIT * 1000);
++		    if (++grab_tries > GRAB_TRIES) {
++			    failed = "keyboard";
++			    goto nograbkb;
++		    }
++	    }
++    }
  	if (grab_server) {
  		gdk_x11_grab_server();
  	}
-@@ -160,7 +165,8 @@ passphrase_dialog(char *message)
- 		XUngrabServer(GDK_DISPLAY());
+@@ -266,7 +269,8 @@ passphrase_dialog(char *message, int prompt_type)
+ 		XUngrabServer(gdk_x11_get_default_xdisplay());
  	if (grab_pointer)
  		gdk_pointer_ungrab(GDK_CURRENT_TIME);
 -	gdk_keyboard_ungrab(GDK_CURRENT_TIME);
