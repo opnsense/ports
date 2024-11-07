3c574e41e4e45df72cbb64c060a5637dd1d64f29
Franco Fichtner <franco@opnsense.org>
2024-11-07 16:48:53 +0100

    ports-mgmt/pkg: a few embedded libfetch updates
    * update according to feedback on recent CRL changes
    * avoid use of bundle to follow base behaviour
    * make CRL missing visible now

--- ports-mgmt/pkg/files/patch-external_libfetch_common.c
+++ ports-mgmt/pkg/files/patch-external_libfetch_common.c
@@ -1,21 +1,34 @@
 --- external/libfetch/common.c.orig	2023-06-29 13:25:53 UTC
 +++ external/libfetch/common.c
-@@ -1089,7 +1089,7 @@ fetch_ssl_setup_peer_verification(SSL_CTX *ctx, int ve
+@@ -1082,8 +1082,6 @@ fetch_ssl_setup_transport_layer(SSL_CTX *ctx, int verb
+ /*
+  * Configure peer verification based on environment.
+  */
+-#define LOCAL_CERT_FILE	"/usr/local/etc/ssl/cert.pem"
+-#define BASE_CERT_FILE	"/etc/ssl/cert.pem"
+ static int
+ fetch_ssl_setup_peer_verification(SSL_CTX *ctx, int verbose)
  {
- 	X509_LOOKUP *crl_lookup;
- 	X509_STORE *crl_store;
--	const char *ca_cert_file, *ca_cert_path, *crl_file;
-+	const char *ca_cert_file, *ca_cert_path, *crl_file, *crl_verify;
+@@ -1093,12 +1091,6 @@ fetch_ssl_setup_peer_verification(SSL_CTX *ctx, int ve
  
  	if (getenv("SSL_NO_VERIFY_PEER") == NULL) {
  		ca_cert_file = getenv("SSL_CA_CERT_FILE");
-@@ -1119,6 +1119,13 @@ fetch_ssl_setup_peer_verification(SSL_CTX *ctx, int ve
+-		if (ca_cert_file == NULL &&
+-		    access(LOCAL_CERT_FILE, R_OK) == 0)
+-			ca_cert_file = LOCAL_CERT_FILE;
+-		if (ca_cert_file == NULL &&
+-		    access(BASE_CERT_FILE, R_OK) == 0)
+-			ca_cert_file = BASE_CERT_FILE;
+ 		ca_cert_path = getenv("SSL_CA_CERT_PATH");
+ 		if (verbose) {
+ 			fetch_info("Peer verification enabled");
+@@ -1119,6 +1111,13 @@ fetch_ssl_setup_peer_verification(SSL_CTX *ctx, int ve
  			    ca_cert_path);
  		else
  			SSL_CTX_set_default_verify_paths(ctx);
-+		if ((crl_verify = getenv("SSL_CRL_VERIFY")) != NULL) {
++		if (getenv("SSL_CRL_VERIFY") != NULL) {
 +			if (verbose)
-+				fetch_info("Using CRL verify: yes");
++				fetch_info("CRL verification enabled");
 +			X509_VERIFY_PARAM_set_flags(SSL_CTX_get0_param(ctx),
 +			    X509_V_FLAG_CRL_CHECK |
 +			    X509_V_FLAG_CRL_CHECK_ALL);
@@ -23,7 +36,7 @@
  		if ((crl_file = getenv("SSL_CRL_FILE")) != NULL) {
  			if (verbose)
  				fetch_info("Using CRL file: %s", crl_file);
-@@ -1189,6 +1196,27 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *
+@@ -1189,6 +1188,27 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *
  	char *str;
  
  	str = NULL;
@@ -42,7 +55,7 @@
 +		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
 +		    (name = X509_get_subject_name(crt)) != NULL)
 +			str = X509_NAME_oneline(name, 0, 0);
-+		fetch_info("No CRL was provided for %s", str);
++		fprintf(stderr, "No CRL was provided for %s\n", str);
 +		OPENSSL_free(str);
 +
 +		verified = 1;
@@ -51,7 +64,7 @@
  	if (!verified) {
  		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
  		    (name = X509_get_subject_name(crt)) != NULL)
-@@ -1197,6 +1225,7 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *
+@@ -1197,6 +1217,7 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *
  		    str != NULL ? str : "no relevant certificate");
  		OPENSSL_free(str);
  	}
