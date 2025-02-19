eae7fcebf89b24e86d6e72c386a55e9ecb9aa069
Franco Fichtner <franco@opnsense.org>
2025-02-19 17:20:45 +0100

    net/ntp: sync with upstream
    Taken from: FreeBSD

--- net/ntp/files/patch-ntpd_ntp__proto.c
+++ net/ntp/files/patch-ntpd_ntp__proto.c
@@ -1,18 +1,11 @@
 --- ntpd/ntp_proto.c.orig	2024-05-07 04:21:28.000000000 -0700
-+++ ntpd/ntp_proto.c	2024-12-23 14:40:06.218503000 -0800
-@@ -471,6 +471,7 @@
- 		return;
- 	}
- 
-+#if 0
++++ ntpd/ntp_proto.c	2025-02-18 19:48:02.135233000 -0800
+@@ -474,7 +474,7 @@
  	/* [Bug 3851] drop pool servers which can no longer be reached. */
  	if (MDF_PCLNT & peer->cast_flags) {
  		if (   (IS_IPV6(&peer->srcadr) && !nonlocal_v6_addr_up)
-@@ -479,6 +480,7 @@
+-		    || !nonlocal_v4_addr_up) {
++		    || (IS_IPV4(&peer->srcadr) && !nonlocal_v4_addr_up)) {
+ 			unpeer(peer);
  			return;
  		}
- 	}
-+#endif
- 
- 	 /*
- 	 * In unicast modes the dance is much more intricate. It is
