57c98eced33572861b7552069a4f5e0bd37aa47e
Franco Fichtner <franco@opnsense.org>
2025-04-22 13:08:23 +0200

    */*: sync with upstream
    Taken from: FreeBSD

---
+++ www/ungoogled-chromium/files/patch-components_webui_flags_flags__state.cc
@@ -0,0 +1,11 @@
+--- components/webui/flags/flags_state.cc.orig	2025-04-15 08:30:07 UTC
++++ components/webui/flags/flags_state.cc
+@@ -787,7 +787,7 @@ unsigned short FlagsState::GetCurrentPlatform() {
+   return kOsWin;
+ #elif BUILDFLAG(IS_CHROMEOS)
+   return kOsCrOS;
+-#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_OPENBSD)
++#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+   return kOsLinux;
+ #elif BUILDFLAG(IS_ANDROID)
+   return kOsAndroid;
--- www/ungoogled-chromium/files/patch-third__party_blink_renderer_modules_webgl_webgl__rendering__context__base.cc
+++ www/ungoogled-chromium/files/patch-third__party_blink_renderer_modules_webgl_webgl__rendering__context__base.cc
@@ -1,6 +1,6 @@
---- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc.orig	2025-03-14 15:54:34 UTC
+--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc.orig	2025-04-17 14:09:48 UTC
 +++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
-@@ -6338,7 +6338,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVid
+@@ -6347,7 +6347,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVid
    constexpr bool kAllowZeroCopyImages = true;
  #endif
  
