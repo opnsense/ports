9521b10081f6ca5f2d0d2686b94ea0cd105d5e03
Franco Fichtner <franco@opnsense.org>
2025-05-13 10:18:03 +0200

    */*: sync with upstream
    Taken from: FreeBSD

---
+++ misc/flag-icons/files/makeflags.sh
@@ -0,0 +1,67 @@
+simple()
+{
+	local comment convert_extra_args destdir height ratio subdir t0 width
+
+	comment=$1; convert_extra_args=$2; destdir=$3; shift 3
+
+	for subdir in ?x?; do
+		ratio=${subdir%x*}/${subdir#*x}
+		for height; do
+			width=$((height * $ratio))
+			mkdir -p "$destdir/${width}x${height}"
+			echo -n "Generating $comment flags ${width}x${height}... "
+			t0=$(date +%s)
+			for flag in $subdir/*.svg; do
+				convert -resize ${width}x${height} $convert_extra_args \
+				"$flag" "$destdir/${width}x${height}/$(basename "$flag" svg)png"
+			done
+			echo "finished in $(date -ur $((`date +%s`-t0)) +%M:%S)"
+		done
+	done
+}
+
+plain()
+{
+	simple undecorated -strip "$@"
+}
+
+bordered()
+{
+	simple black-bordered '-shave 1x1 -bordercolor black -border 1 -strip' "$@"
+}
+
+# https://joeldare.com/rounding-image-corners-with-imagemagick
+rounded()
+{
+	local destdir height mask radius ratio subdir t0 width
+
+	destdir=$1; shift
+	mask=$(mktemp)
+
+	for subdir in ?x?; do
+		ratio=${subdir%x*}/${subdir#*x}
+		for height; do
+			width=$((height * $ratio))
+			radius=$((height / 10))
+
+			convert -size ${width}x${height} xc:none -draw \
+			"roundRectangle 0,0,$((width-1)),$((height-1)),$radius,$radius" \
+			-strip "png:$mask"
+
+			mkdir -p "$destdir/${width}x${height}"
+
+			echo -n "Generating rounded corner flags ${width}x${height}... "
+			t0=$(date +%s)
+			for flag in $subdir/*.svg; do
+				convert -resize ${width}x${height} \
+				"$flag" -matte "$mask" \
+				-compose DstIn -composite -strip \
+				"$destdir/${width}x${height}/$(basename "$flag" svg)png"
+			done
+			echo "finished in $(date -ur $((`date +%s`-t0)) +%M:%S)"
+		done
+	done
+	rm "$mask"
+}
+
+"$@"
