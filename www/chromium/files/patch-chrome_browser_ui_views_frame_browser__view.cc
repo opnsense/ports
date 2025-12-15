1814f5b728b5657c8fd08e3287f002ab3bd49052
Franco Fichtner <franco@opnsense.org>
2025-12-15 13:42:58 +0100

    */*: sync with upstream
    Taken from: FreeBSD

--- www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
+++ www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
@@ -1,4 +1,4 @@
---- chrome/browser/ui/views/frame/browser_view.cc.orig	2025-12-05 10:12:50 UTC
+--- chrome/browser/ui/views/frame/browser_view.cc.orig	2025-12-12 12:11:50 UTC
 +++ chrome/browser/ui/views/frame/browser_view.cc
 @@ -2305,7 +2305,7 @@ void BrowserView::ToolbarSizeChanged(bool is_animating
  }
@@ -9,7 +9,7 @@
    std::vector<ContentsWebView*> contents_web_views =
        GetAllVisibleContentsWebViews();
  
-@@ -5919,7 +5919,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
+@@ -5920,7 +5920,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
  }
  
  void BrowserView::MaybeShowSupervisedUserProfileSignInIPH() {
@@ -18,7 +18,7 @@
    if (!ShouldShowAvatarToolbarIPH()) {
      return;
    }
-@@ -5929,7 +5929,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
+@@ -5930,7 +5930,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
  }
  
  void BrowserView::MaybeShowSignInBenefitsIPH() {
