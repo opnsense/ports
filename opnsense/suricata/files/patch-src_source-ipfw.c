8032100ce36042d6047b2c9450392f6e0ffceb9f
Franco Fichtner <franco@opnsense.org>
2026-02-07 20:24:22 +0100

    opnsense/suricata: exclude two seemingly normal errors from being fatal

--- opnsense/suricata/files/patch-src_source-ipfw.c
+++ opnsense/suricata/files/patch-src_source-ipfw.c
@@ -1,4 +1,4 @@
---- src/source-ipfw.c.orig	2025-11-05 14:48:20 UTC
+--- src/source-ipfw.c.orig	2026-01-09 15:27:36 UTC
 +++ src/source-ipfw.c
 @@ -360,6 +360,14 @@ TmEcode ReceiveIPFWThreadInit(ThreadVars *tv, const vo
          SCReturnInt(TM_ECODE_FAILED);
@@ -15,3 +15,14 @@
      nq->ipfw_sinlen=sizeof(nq->ipfw_sin);
      memset(&nq->ipfw_sin, 0, nq->ipfw_sinlen);
      nq->ipfw_sin.sin_family = PF_INET;
+@@ -570,6 +578,10 @@ TmEcode IPFWSetVerdict(ThreadVars *tv, IPFWThreadVars 
+                     SCReturnInt(TM_ECODE_FAILED);
+                 case EHOSTDOWN:
+                 case ENETDOWN:
++#ifdef PF_DIVERT
++                case EHOSTUNREACH:
++                case ENETUNREACH:
++#endif
+                     break;
+             }
+         }
