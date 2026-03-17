5db0bccec7c8f850a2bc67e2fdef104552a995b8
Franco Fichtner <franco@opnsense.org>
2026-03-17 10:57:03 +0100

    */*: sync with upstream
    Taken from: FreeBSD

---
+++ multimedia/ffmpeg6/files/patch-libavutil-x86-asm.h
@@ -0,0 +1,11 @@
+--- libavutil/x86/asm.h.orig	2023-11-09 23:38:51 UTC
++++ libavutil/x86/asm.h
+@@ -72,7 +72,7 @@ typedef int x86_reg;
+ typedef int x86_reg;
+ #endif
+ 
+-#define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE))
++#define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE && !defined(__clang__)))
+ #define HAVE_6REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE || HAVE_EBP_AVAILABLE))
+ 
+ #if ARCH_X86_64 && defined(PIC)
